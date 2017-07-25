ef6b8f4a1b8b12ba628d249209971538d2d44a9e
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2017-07-25 12:23:46 +0500

    use slim instead slime

--- config/config.exs
+++ config/config.exs
@@ -25,6 +25,8 @@ config :logger, :console,
 config :phoenix, :template_engines,
   slim: PhoenixSlime.Engine,
   slime: PhoenixSlime.Engine
+
+config :phoenix_slime, :use_slim_extension, true
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
