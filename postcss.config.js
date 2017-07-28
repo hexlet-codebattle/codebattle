46e53317231ecdba2b91feee9875ab48f6db9707
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2017-07-28 18:11:21 +0500

    add postcss config file

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    require('postcss-flexbugs-fixes'),
+    require('autoprefixer')({
+      browsers: [
+        'Chrome >= 35',
+        'Firefox >= 38',
+        'Edge >= 12',
+        'Explorer >= 10',
+        'iOS >= 8',
+        'Safari >= 8',
+        'Android 2.3',
+        'Android >= 4',
+        'Opera >= 12'
+      ]
+    }),
+  ]
+}
