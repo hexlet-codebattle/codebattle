8efb7e1380df333934602b85e4d3bcf31d3c34be
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2017-08-04 02:34:13 +0500

    add eslint

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,11 +11,11 @@
 //
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
-import "phoenix_html"
+import 'phoenix_html';
 
 // Import local files
 //
 // Local files can be imported directly using relative
 // paths "./socket" or full ones "web/static/js/socket".
 
-import socket from "./socket"
+import socket from './socket';
