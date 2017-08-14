1d8dd50c4b82bb827e6942cbcf2a7d6a5b8d4b27
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2017-08-14 16:42:50 +0500

    fix crash while creating game

--- lib/codebattle_web/views/game_view.ex
+++ lib/codebattle_web/views/game_view.ex
@@ -1,3 +1,8 @@
 defmodule CodebattleWeb.GameView do
   use Codebattle.Web, :view
+
+  def get_users(game) do
+    [game.data.first_player, game.data.second_player]
+    |> Enum.filter(fn x -> x end)
+  end
 end
