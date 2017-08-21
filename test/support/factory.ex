5808569b0df4a49a0dd16f3a317132984e00ffb3
PeresvetS <niks280593@gmail.com>
2017-08-21 17:13:32 +0300

    update tests

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,13 +1,27 @@
 defmodule CodebattleWeb.Factory do
   use ExMachina.Ecto, repo: Codebattle.Repo
 
+
   alias Codebattle.User
+  alias Ueberauth.Auth
 
   def user_factory do
     %User{
       name: sequence(:username, &"User #{&1}"),
       email: "test@test.io",
       github_id: :rand.uniform(9999999)
+    }
+  end
+
+  def auth_factory do
+    %Auth{
+      provider: :github,
+      uid: :rand.uniform(100_000),
+      extra: %{
+        raw_info: %{
+          user: %{},
+        },
+      },
     }
   end
 end
