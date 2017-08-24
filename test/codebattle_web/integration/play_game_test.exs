6096e462be87689a1c27d4d02df3cebd1ca4291a
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2017-08-25 01:03:21 +0500

    fix integration test

--- test/codebattle_web/integration/play_game_test.exs
+++ test/codebattle_web/integration/play_game_test.exs
@@ -12,7 +12,7 @@ defmodule Codebattle.PlayGameTest do
     {:ok, %{user1_conn: user1_conn, user2_conn: user2_conn, user3_conn: user3_conn, user1: user1, user2: user2, user3: user3}}
   end
 
-  test "Two users play game", %{user1_conn: user1_conn, user2_conn: user2_conn, user3_conn: user3_conn, user1: user1, user2: user2, user3: user3} do
+  test "Two users play game", %{user1_conn: user1_conn, user2_conn: user2_conn, user1: user1, user2: user2} do
 
     # Create game
     conn = post(user1_conn, game_path(user1_conn, :create))
@@ -53,28 +53,6 @@ defmodule Codebattle.PlayGameTest do
     assert fsm.data.loser == nil
     assert fsm.data.game_over == false
 
-    # Other player cannot join game
-    post(user3_conn, game_location <> "/join")
-    fsm = Play.Server.fsm(game_id)
-
-    assert fsm.state == :playing
-    assert fsm.data.first_player.name == "first"
-    assert fsm.data.second_player.name == "second"
-    assert fsm.data.winner == nil
-    assert fsm.data.loser == nil
-    assert fsm.data.game_over == false
-
-    # Other player cannot win game
-    post(user3_conn, game_location <> "/check")
-    fsm = Play.Server.fsm(game_id)
-
-    assert fsm.state == :playing
-    assert fsm.data.first_player.name == "first"
-    assert fsm.data.second_player.name == "second"
-    assert fsm.data.winner == nil
-    assert fsm.data.loser == nil
-    assert fsm.data.game_over == false
-
     # First player won
     post(user1_conn, game_location <> "/check")
     fsm = Play.Server.fsm(game_id)
@@ -96,5 +74,37 @@ defmodule Codebattle.PlayGameTest do
     assert game.state == "game_over"
     assert user1.raiting == 11
     assert user2.raiting == 9
+  end
+
+  test "other players cannot change game state", %{user1_conn: user1_conn, user2_conn: user2_conn, user3_conn: user3_conn} do
+    conn = post(user1_conn, game_path(user1_conn, :create))
+    game_location = conn.resp_headers
+                    |> Enum.find(&match?({"location", _}, &1))
+                    |> elem(1)
+    game_id = ~r/\d+/ |> Regex.run(game_location) |> List.first |> String.to_integer
+
+    post(user2_conn, game_location <> "/join")
+
+    # Other player cannot join game
+    post(user3_conn, game_location <> "/join")
+    fsm = Play.Server.fsm(game_id)
+
+    assert fsm.state == :playing
+    assert fsm.data.first_player.name == "first"
+    assert fsm.data.second_player.name == "second"
+    assert fsm.data.winner == nil
+    assert fsm.data.loser == nil
+    assert fsm.data.game_over == false
+
+    # Other player cannot win game
+    post(user3_conn, game_location <> "/check")
+    fsm = Play.Server.fsm(game_id)
+
+    assert fsm.state == :playing
+    assert fsm.data.first_player.name == "first"
+    assert fsm.data.second_player.name == "second"
+    assert fsm.data.winner == nil
+    assert fsm.data.loser == nil
+    assert fsm.data.game_over == false
   end
 end
