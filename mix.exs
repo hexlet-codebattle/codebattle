6492da4fa2edaaae310830b6a15043dcbdb040ce
vtm <vtmilyakov@yandex.ru>
2017-12-03 20:17:47 +0300

    Bump 0.0.8

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Codebattle.Mixfile do
 
   def project do
     [app: :codebattle,
-     version: "0.0.7",
+     version: "0.0.8",
      elixir: "~> 1.5",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
