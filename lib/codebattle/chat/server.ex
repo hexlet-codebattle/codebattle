1aed4a02a2a2c95ede6ae524e541c2549e641c77
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2017-12-04 03:32:54 +0500

    get users from chat server

--- lib/codebattle/chat/server.ex
+++ lib/codebattle/chat/server.ex
@@ -5,7 +5,28 @@ defmodule Codebattle.Chat.Server do
         GenServer.start_link(__MODULE__, [], name: chat_key(id))
     end
 
+    def get_users(id) do
+        GenServer.call(chat_key(id), :get_users)
+    end
+
+    def join_chat(id, user) do
+        GenServer.call(chat_key(id), {:join, user})
+    end
+
     defp chat_key(id) do
         {:via, :gproc, {:n, :l, {:chat, to_charlist(id)}}}
+    end
+
+    def init(_) do
+        {:ok, %{users: [], msgs: []}}
+    end
+
+    def handle_call({:join, _user}, _from, state) do
+        {:reply, state, state}
+    end
+
+    def handle_call(:get_users, _from, state) do
+        %{users: users} = state
+        {:reply, users, state}
     end
 end
