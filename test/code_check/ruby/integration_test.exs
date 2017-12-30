5064890c5978a67e26dd3637999cac24e378640d
vtm <vtmilyakov@yandex.ru>
2017-12-30 14:17:12 +0300

    Remove docker tests from travis

--- test/code_check/ruby/integration_test.exs
+++ test/code_check/ruby/integration_test.exs
@@ -1,4 +1,4 @@
-defmodule Codebattle.CodeCheck.JS.IntegrationTest do
+defmodule Codebattle.CodeCheck.Ruby.IntegrationTest do
   use Codebattle.IntegrationCase
 
   alias CodebattleWeb.GameChannel
@@ -16,7 +16,6 @@ defmodule Codebattle.CodeCheck.JS.IntegrationTest do
     {:ok, %{user1: user1, user2: user2, task: task, socket1: socket1, socket2: socket2}}
   end
 
-  @tag :skip
   test "bad code, game playing", %{user1: user1, user2: user2, task: task, socket1: socket1, socket2: socket2} do
     #setup
     state = :playing
@@ -29,7 +28,7 @@ defmodule Codebattle.CodeCheck.JS.IntegrationTest do
     :lib.flush_receive()
 
     push socket1, "editor:text", %{editor_text: "dsf"}
-    push socket1, "check_result", %{editor_text: "sdf", lang: "js"}
+    push socket1, "check_result", %{editor_text: "sdf", lang: "ruby"}
     :timer.sleep(2_000)
 
     fsm = Server.fsm(game.id)
@@ -37,7 +36,6 @@ defmodule Codebattle.CodeCheck.JS.IntegrationTest do
     assert fsm.state == :playing
   end
 
-  @tag :skip
   test "good code, player won", %{user1: user1, user2: user2, task: task, socket1: socket1, socket2: socket2} do
     #setup
     state = :playing
@@ -51,36 +49,14 @@ defmodule Codebattle.CodeCheck.JS.IntegrationTest do
 
     push socket1, "editor:text", %{editor_text: "test"}
     push socket1, "check_result", %{
-      editor_text: "export default (a,b) => { return a + b; }",
-      lang: "js"
+      editor_text: "def solution(x,y); x + y; end",
+      lang: "ruby"
     }
-
-    :timer.sleep 3_000
-
-    fsm = Server.fsm(game.id)
-
-    assert fsm.state == :player_won
-  end
-
-  @tag :skip
-  test "process exit do not working", %{user1: user1, user2: user2, task: task, socket1: socket1, socket2: socket2} do
-    #setup
-    state = :playing
-    data = %{first_player: user1, second_player: user2, task: task}
-    game = setup_game(state, data)
-    game_topic = "game:" <> to_string(game.id)
-
-    {:ok, _response, socket1} = subscribe_and_join(socket1, GameChannel, game_topic)
-    {:ok, _response, _socket2} = subscribe_and_join(socket2, GameChannel, game_topic)
-    :lib.flush_receive()
-
-    push socket1, "editor:text", %{editor_text: "test"}
-    push socket1, "check_result", %{editor_text: "process.exit()", lang: "js"}
 
     :timer.sleep 2_000
 
     fsm = Server.fsm(game.id)
 
-    assert fsm.state == :playing
+    assert fsm.state == :player_won
   end
 end
