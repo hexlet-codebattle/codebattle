1c5dfe9fc05cbdb7c096a84ed4e9741f46e1299d
vtm <vtmilyakov@yandex.ru>
2017-12-30 21:29:04 +0300

    Test live reload which_children

--- lib/codebattle/chat/supervisor.ex
+++ lib/codebattle/chat/supervisor.ex
@@ -1,8 +1,7 @@
 defmodule Codebattle.Chat.Supervisor do
     @moduledoc false
-
     use Supervisor
-    
+
     def start_link do
         Supervisor.start_link(__MODULE__, [], name: __MODULE__)
     end
