481175fb2326627527ad5c001b315ef1ec3029b2
vtm <vtmilyakov@yandex.ru>
2017-12-31 16:24:12 +0300

    Add logger to checker

--- lib/codebattle/code_check/checker.ex
+++ lib/codebattle/code_check/checker.ex
@@ -9,6 +9,7 @@ defmodule Codebattle.CodeCheck.Checker do
     {dir_path, check_code} = prepare_tmp_dir!(task, editor_text, language)
 
     {global_output, status} = System.cmd("make", ["run"], cd: dir_path, stderr_to_stdout: true)
+    Logger.info "Docker stdout lang: #{language}, output:#{global_output}"
     [_h1 | [_h2 | output]] = String.split(global_output, "\n")
     result = case  {output, status} do
      {[^check_code], 0} ->
