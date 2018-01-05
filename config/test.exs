6ef50f18cd107c06d9ccb95997fb00966c60b784
vtm <vtmilyakov@yandex.ru>
2018-01-05 15:47:33 +0300

    Fix test env

--- config/test.exs
+++ config/test.exs
@@ -15,10 +15,10 @@ config :logger, level: :warn
 # Configure your database
 config :codebattle, Codebattle.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "",
+  username: System.get_env("CODEBATTLE_DATABASE_USERNAME"),
+  password: System.get_env("CODEBATTLE_DATABASE_PASSWORD"),
   database: "codebattle_test",
-  hostname: "db",
+  hostname: System.get_env("CODEBATTLE_DATABASE_HOSTNAME"),
   pool: Ecto.Adapters.SQL.Sandbox,
   ownership_timeout: 99_999_999
 
