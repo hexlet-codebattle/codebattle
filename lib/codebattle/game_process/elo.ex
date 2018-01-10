c90099788ac46f6f0fc3314439989ec7c6efed4b
Roman Pushkov <pushkovroman@me.com>
2018-01-10 20:02:09 +0300

    elo update

--- lib/codebattle/game_process/elo.ex
+++ lib/codebattle/game_process/elo.ex
@@ -1,21 +1,32 @@
 defmodule Codebattle.GameProcess.Elo do
+  @moduledoc """
+    Elo
+  """
+
+  @kvalues %{
+    "elementary" => 16,
+    "easy" => 24,
+    "medium" => 32,
+    "hard" => 40
+  }
+
   @doc """
     Calculates Elo rating
 
   ## Examples
 
-    iex(1)> Codebattle.GameProcess.Elo.calc_elo(1000, 1000)
-    %{loser: 984, winner: 1016}
+    iex(1)> Codebattle.GameProcess.Elo.calc_elo(1000, 1000, 32)
+    {1016, 984}
   
   """
-  def calc_elo(winner_rating, loser_rating, k \\ 32) do
-    %{
-      winner: winner_rating + k * winner_expected(winner_rating, loser_rating) |> round,
-      loser: loser_rating + k * loser_expected(winner_rating, loser_rating) |> round
+  def calc_elo(winner_rating, loser_rating, difficulty \\ "medium") do
+    {
+      round(winner_rating + @kvalues[difficulty] * winner_expected(winner_rating, loser_rating)),
+      round(loser_rating + @kvalues[difficulty] * loser_expected(winner_rating, loser_rating))
     }
   end
   
-  defp transform_rating(rating), do: :math.pow(10, ( rating / 400 ))
-  defp winner_expected(r1, r2), do: 1 - ( transform_rating(r1) / ( transform_rating(r1) + transform_rating(r2) ) )
-  defp loser_expected(r1, r2), do: 0 - ( transform_rating(r2) / ( transform_rating(r1) + transform_rating(r2) ) )
+  defp transform_rating(rating), do: :math.pow(10, (rating / 400))
+  defp winner_expected(r1, r2), do: 1 - (transform_rating(r1) / (transform_rating(r1) + transform_rating(r2)))
+  defp loser_expected(r1, r2), do: 0 - (transform_rating(r2) / (transform_rating(r1) + transform_rating(r2)))
 end
