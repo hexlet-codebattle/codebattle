6f5d1f7b7643b560dc9b1dbe8c2118f1dbcf7533
Roman Pushkov <pushkovroman@me.com>
2018-02-05 21:06:37 +0300

    tests update

--- services/app/test/codebattle_web/controllers/user_controller_test.exs
+++ services/app/test/codebattle_web/controllers/user_controller_test.exs
@@ -20,7 +20,7 @@ defmodule CodebattleWeb.UserControllerTest do
     assert redirected_to(conn, 302) == "/"
   end
 
-  test "show user profile: signed in", %{conn: conn} do
+  test "show user: signed in", %{conn: conn} do
     user = insert(:user)
 
     conn =
@@ -31,7 +31,7 @@ defmodule CodebattleWeb.UserControllerTest do
     assert conn.status == 200
   end
 
-  test "show user profile: not signed in", %{conn: conn} do
+  test "show user: not signed in", %{conn: conn} do
     user = insert(:user)
 
     conn =
@@ -39,38 +39,5 @@ defmodule CodebattleWeb.UserControllerTest do
       |> get(user_path(conn, :show, user.id))
 
     assert redirected_to(conn, 302) == "/"
-  end
-
-  test "edit user profile: signed in", %{conn: conn} do
-    user = insert(:user)
-
-    conn =
-      conn
-      |> put_session(:user_id, user.id)
-      |> get(user_path(conn, :edit, user.id))
-
-    assert conn.status == 200
-  end
-
-  test "edit user profile: not signed in", %{conn: conn} do
-    user = insert(:user)
-
-    conn =
-      conn
-      |> get(user_path(conn, :edit, user.id))
-
-    assert redirected_to(conn, 302) == "/"
-  end
-
-  test "edit user profile: different user", %{conn: conn} do
-    user_owner = insert(:user)
-    user_visitor = insert(:user)
-
-    conn =
-      conn
-      |> put_session(:user_id, user_visitor.id)
-      |> get(user_path(conn, :edit, user_owner.id))
-
-    assert redirected_to(conn, 302) == "/users/#{user_owner.id}"
   end
 end
