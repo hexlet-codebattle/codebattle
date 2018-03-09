14808aa637c09cf581a6891a5cf11bb99234066c
vtm <vtmilyakov@yandex.ru>
2018-03-09 16:48:36 +0300

    Add new task for uploaad langs

--- make-production.mk
+++ make-production.mk
@@ -7,8 +7,11 @@ production-env-update:
 	ansible-playbook ansible/deploy.yml -i ansible/production -u $U --tag env
 
 production-deploy:
-	ansible-playbook ansible/deploy.yml -i ansible/production -u $U --ask-vault-pass
+	ansible-playbook ansible/deploy.yml -i ansible/production -u $U --ask-vault-pass --ask-become-pass
 
 production-build-and-push:
 	docker build -t codebattle/app --file services/app/Dockerfile.prod services/app/
 	docker push codebattle/app
+
+production-upload-langs:
+	ansible-playbook ansible/langs.yml -i ansible/production -u $U -vv --ask-vault-pass
