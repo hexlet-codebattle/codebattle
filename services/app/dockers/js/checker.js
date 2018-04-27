e25ba7c60b4b8e9c9b9bb065e142dcd080664d41
Artem Arefiev <a.arefiev@inbox.ru>
2018-04-27 10:19:38 +0400

    Add test result in JSON format for JS

--- services/app/dockers/js/checker.js
+++ services/app/dockers/js/checker.js
@@ -1,32 +1,41 @@
-const solution = require("./check/solution");
-
 const readline = require("readline");
 const chai = require("chai");
 
 const assert = chai.assert;
 
-var rl = readline.createInterface({
+const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
   terminal: false
 });
 
-var checks = [];
+const checks = [];
 
 rl.on("line", function(line) {
   // console.log(line);
   checks.push(JSON.parse(line));
-})
+});
 
 rl.on("close", function() {
+  try {
+    const solution = require("./check/solution");
 
-  checks.forEach(function(check) {
-    if (check["check"]) {
-      process.stdout.write(check["check"]);
-    } else {
-      var result = solution.apply(null, check.arguments);
-      const msg = check.arguments.map(function(arg) { return JSON.stringify(arg) }).join(", ");
-      assert.deepEqual(result, check.expected, "Arguments was: (" + msg + ")");
-    }
-  });
+    checks.forEach(function(check) {
+      if (check.check) {
+        process.stdout.write(JSON.stringify({
+          status: "ok",
+          result: check.check
+        }));
+      } else {
+        const result = solution.apply(null, check.arguments);
+
+        assert.equal(result, check.expected, JSON.stringify(check.arguments));
+      }
+    });
+  } catch (e) {
+    process.stdout.write(JSON.stringify({
+      status: "error",
+      result: e.message
+    }));
+  }
 });
