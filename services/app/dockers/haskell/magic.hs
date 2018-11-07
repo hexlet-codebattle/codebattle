ad3414408d98c0c39e456927a59b9115ca5e8221
Abbath <pitongogi@gmail.com>
2018-11-07 21:11:41 +0100

    Remove non-unicode characters from response

--- services/app/dockers/haskell/magic.hs
+++ services/app/dockers/haskell/magic.hs
@@ -1,15 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where 
 
-import  System.Exit
-import  System.Process.Typed
-import  qualified Data.ByteString as BS (isInfixOf)
-import  qualified Data.ByteString.Lazy.Char8 as BSL (toStrict, unpack)
+import System.Exit
+import Data.Char
+import System.Process.Typed
+import qualified Data.ByteString as BS (isInfixOf)
+import qualified Data.ByteString.Lazy.Char8 as BSL (filter, unpack)
   
 main = do
     (exitCode, dateOut, dateErr) <- readProcess "cabal new-build Checker"
     case exitCode of 
         ExitSuccess -> return ()
         (ExitFailure _)-> do 
-            print $ "{ \"status\": \"error\", \"result\":\"" ++ BSL.unpack dateErr ++ "\"}"
+            putStrLn $ "{ \"status\": \"error\", \"result\":\"" ++ BSL.unpack (BSL.filter (\c -> ord c < 128) dateErr) ++ "\"}"
             exitWith (ExitFailure 1)
