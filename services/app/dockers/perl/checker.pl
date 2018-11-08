84601414c4713b5d54efca12547074d65c0b2bad
Abbath <pitongogi@gmail.com>
2018-11-08 19:27:35 +0100

    Perl added

--- services/app/dockers/perl/checker.pl
+++ services/app/dockers/perl/checker.pl
@@ -1,15 +1,23 @@
 use strict;
 use warnings;
-require "./solution.pl";
+use JSON::MaybeXS qw(encode_json decode_json); 
+require "./check/solution.pl";
+
+$SIG{__DIE__} = sub {print "{\"status\":\"error\", \"result\":\"unexpected\"}\n"; exit 1;};
+$SIG{__WARN__} = sub {print "{\"status\":\"error\", \"result\":\"unexpected\"}\n"; exit 1;};
 
 while(<>){
     my $JSON=$_;
-    $JSON=~s/:/=>/g;
-    my $PERL=eval $JSON;
+    my $PERL = decode_json $JSON;
     if (exists $PERL->{check}){
+        print "{\"status\":\"ok\", \"result\" : \"" . $PERL->{check} . "\"}\n";
         last;
     }
     my $args = $PERL->{arguments};
     my $exp = $PERL->{expected};
-    print solution(@$args) == $exp;
+    if (solution(@$args) != $exp) {
+        print "{\"status\":\"failure\", \"result\":" . encode_json($PERL->{arguments}) . "}\n";
+        last;
+    }
 }
+
