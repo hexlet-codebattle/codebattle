5ec7036ad5b6342ab4aa39055b57741b4ebb2510
vtm9 <vtmilyakov@yandex.ru>
2019-01-01 21:25:32 +0300

    --wip-- [skip ci]

--- services/app/priv/repo/migrations/20190101145738_add_public_user_id_uuid.exs
+++ services/app/priv/repo/migrations/20190101145738_add_public_user_id_uuid.exs
@@ -2,8 +2,6 @@ defmodule Codebattle.Repo.Migrations.AddPublicUserIdUuid do
   use Ecto.Migration
 
   def change do
-    execute("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";")
-
     alter table(:users) do
       add(:public_id, :uuid, default: fragment("uuid_generate_v4()"))
     end
