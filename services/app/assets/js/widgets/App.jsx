b644d7cd107397c8cc7892558eaff3b295a3d88d
skhrv <notebook1v@gmail.com>
2019-01-20 21:47:54 +0500

    add redux-persist

--- services/app/assets/js/widgets/App.jsx
+++ services/app/assets/js/widgets/App.jsx
@@ -1,22 +1,39 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
 import RootContainer from './containers/RootContainer';
 import createStore from './lib/configureStore';
 import reducers from './reducers';
 import GameList from './containers/GameList';
 
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 // TODO: put initial state from gon
-const store = createStore(reducers, {});
+const store = createStore(persistedReducer, {});
+
+const persistor = persistStore(store);
 
 export const Game = () => (
   <Provider store={store}>
-    <RootContainer />
+    <PersistGate loading={null} persistor={persistor}>
+      <RootContainer />
+    </PersistGate>
   </Provider>
 );
 
 
 export const Lobby = () => (
   <Provider store={store}>
-    <GameList />
+    <PersistGate loading={null} persistor={persistor}>
+      <GameList />
+    </PersistGate>
   </Provider>
 );
