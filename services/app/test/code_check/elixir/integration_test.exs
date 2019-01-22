67ad98663a7bc5a84970ff37ac4e67b0298103cb
vtm9 <vtmilyakov@yandex.ru>
2019-01-22 10:22:46 +0300

    Finally fix tests. Yay

--- services/app/test/code_check/elixir/integration_test.exs
+++ services/app/test/code_check/elixir/integration_test.exs
@@ -3,6 +3,7 @@ defmodule Codebattle.CodeCheck.Elixir.IntegrationTest do
 
   alias CodebattleWeb.GameChannel
   alias Codebattle.GameProcess.{Server, Player}
+  alias CodebattleWeb.UserSocket
 
   setup do
     timeout = Application.fetch_env!(:codebattle, :code_check_timeout)
@@ -12,8 +13,8 @@ defmodule Codebattle.CodeCheck.Elixir.IntegrationTest do
 
     task = insert(:task)
 
-    socket1 = socket("user_id", %{user_id: user1.id, current_user: user1})
-    socket2 = socket("user_id", %{user_id: user2.id, current_user: user2})
+    socket1 = socket(UserSocket, "user_id", %{user_id: user1.id, current_user: user1})
+    socket2 = socket(UserSocket, "user_id", %{user_id: user2.id, current_user: user2})
 
     {:ok,
      %{
@@ -49,15 +50,16 @@ defmodule Codebattle.CodeCheck.Elixir.IntegrationTest do
     {:ok, _response, _socket2} = subscribe_and_join(socket2, GameChannel, game_topic)
     Mix.Shell.Process.flush()
 
-    ref =
-      Phoenix.ChannelTest.push(socket1, "check_result", %{
-        editor_text: "defmodule Solution do\n def solution(x,y) do; x - y; end end",
-        lang: "elixir"
-      })
+    Phoenix.ChannelTest.push(socket1, "check_result", %{
+      editor_text: "defmodule Solution do\n def solution(x,y) do; x - y; end end",
+      lang: "elixir"
+    })
 
     :timer.sleep(timeout)
 
-    assert_reply(ref, :ok, %{output: output, result: result})
+    assert_receive %Phoenix.Socket.Broadcast{
+      payload: %{result: result, output: output}
+    }
 
     expected_result = %{"status" => "failure", "result" => [1, 1]}
     assert expected_result == Poison.decode!(result)
@@ -90,10 +92,12 @@ defmodule Codebattle.CodeCheck.Elixir.IntegrationTest do
     {:ok, _response, _socket2} = subscribe_and_join(socket2, GameChannel, game_topic)
     Mix.Shell.Process.flush()
 
-    ref = Phoenix.ChannelTest.push(socket1, "check_result", %{editor_text: "sdf", lang: "elixir"})
+    Phoenix.ChannelTest.push(socket1, "check_result", %{editor_text: "sdf", lang: "elixir"})
     :timer.sleep(timeout)
 
-    assert_reply(ref, :ok, %{output: output, result: result})
+    assert_receive %Phoenix.Socket.Broadcast{
+      payload: %{result: result, output: output}
+    }
 
     expected_result = %{"status" => "error", "result" => "undefined function sdf/0"}
 
