bf288b35128aa50ac3d8b80a3a34b4e0e3596f40
mimikria96 <chemiakina7493@mail.ru>
2019-02-12 23:48:57 +0300

    fix achievements

--- services/app/lib/codebattle/user/achievements.ex
+++ services/app/lib/codebattle/user/achievements.ex
@@ -39,6 +39,7 @@ defmodule Codebattle.User.Achievements do
         else
           {achievements ++ ["played_hundred_games"], user}
         end
+      true -> {achievements, user}
     end
   end
 
