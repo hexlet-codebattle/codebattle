70886da30a5e66e27e9bac6ba406d9497bcff7d3
vtm9 <vtmilyakov@yandex.ru>
2019-06-28 13:06:09 +0300

    Move rank to backend

--- services/app/lib/codebattle_web/controllers/api/v1/user_controller.ex
+++ services/app/lib/codebattle_web/controllers/api/v1/user_controller.ex
@@ -23,46 +23,49 @@ defmodule CodebattleWeb.Api.V1.UserController do
     page_number = Map.get(params, "page", "1")
     filter = Map.get(params, "filter")
 
+    subquery =
+      from(u in User,
+        order_by: {:desc, :rating},
+        left_join: ug in UserGame,
+        on: u.id == ug.user_id,
+        group_by: u.id,
+        select: %User{
+          id: u.id,
+          name: u.name,
+          rating: u.rating,
+          github_id: u.github_id,
+          lang: u.lang,
+          games_played: count(ug.user_id),
+          rank: fragment("row_number() OVER(order by ? desc)", u.rating)
+        }
+      )
+
     query =
       case filter do
         nil ->
-          from(u in User,
-            order_by: {:desc, :rating},
-            join: ug in UserGame,
-            on: u.id == ug.user_id,
-            group_by: u.id,
-            select: [u.name, u.id, u.rating, u.github_id, u.lang, count(ug.user_id)]
-          )
+          subquery
+
+        "" ->
+          subquery
 
         _ ->
-          from(u in User,
-            order_by: {:desc, :rating},
-            join: ug in UserGame,
-            on: u.id == ug.user_id,
-            group_by: u.id,
-            where: ilike(u.name, ^"%#{filter}%"),
-            select: [u.name, u.id, u.rating, u.github_id, u.lang, count(ug.user_id)]
-          )
+          a =
+            from(t in subquery(subquery),
+              where: ilike(t.name, ^"%#{filter}%")
+            )
       end
 
     page = Repo.paginate(query, %{page: page_number})
 
+    page_info = Map.take(page, [:page_number, :page_size, :total_entries, :total_pages])
+
     users =
       Enum.map(
         page.entries,
-        fn [name, id, rating, github_id, lang, game_count] ->
-          %{
-            name: name,
-            id: id,
-            github_id: github_id,
-            rating: rating,
-            lang: lang,
-            game_count: game_count
-          }
+        fn user ->
+          Map.put(user, :performance, user.rating / user.games_played)
         end
       )
-
-    page_info = Map.take(page, [:page_number, :page_size, :total_entries, :total_pages])
 
     json(conn, %{users: users, page_info: page_info})
   end
