ff554fdc964bd2d39a4fe0ba8cecb2c4d6f23c84
Bataev Dmitriy <bataevdmitriy@gmail.com>
2019-07-08 19:51:20 +0300

    update checker

--- services/app/lib/codebattle/code_check/checker.ex
+++ services/app/lib/codebattle/code_check/checker.ex
@@ -7,7 +7,7 @@ defmodule Codebattle.CodeCheck.Checker do
   alias Codebattle.Generators.CheckerGenerator
   # alias Codebattle.CodeCheck.OutputFilter
 
-  @advanced_checker_lang_list ["ts"]
+  @advanced_checker_lang_list ["ts", "golang"]
 
   def check(task, editor_text, editor_lang) do
     case Languages.meta() |> Map.get(editor_lang) do
