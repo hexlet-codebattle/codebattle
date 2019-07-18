9bec189563f43fbfa9ce15231a3f11eef3868f21
rdb <rdb@rdb-desktop>
2019-07-18 23:09:46 +0300

    update example and template

--- services/app/dockers/haskell/checker_example.hs
+++ services/app/dockers/haskell/checker_example.hs
@@ -8,7 +8,8 @@ import Check.Solution
 import System.Exit
 
 output = BS.putStrLn . A.encode . A.object
-handleSuccess res = output ["status" A..= ("success" :: String), "result" A..= res]
+handleOk = output ["status" A..= ("ok" :: String), "result" A..= ("__check_0__" :: String)]
+handleSuccess res = output ["status" A..= ("ok" :: String), "result" A..= res]
 handleFailure res args = output ["status" A..= ("failure" :: String), "result" A..= res, "arguments" A..= args]
 handleRuntimeError e = output ["status" A..= ("error" :: String), "result" A..= show e]
 
@@ -16,16 +17,26 @@ main :: IO ()
 main = do
     let expected1 = 2
     let res1 = solution 1 1
+    let resAssert1 = expected2 == solution 1 1
 
-    (if res1 == expected1
+    (if resAssert1
         then handleSuccess res1
         else handleFailure res1 ("[1, 1]" :: String))
         `catch` \(e ::ErrorCall) -> handleRuntimeError e
 
     let expected2 = 8
-    let res2 = solution 5 3
+    let res2 = solution 1 1
+    let resAssert2 = expected2 == solution 5 3
 
-    (if res2 == expected2
+    (if resAssert2
         then handleSuccess res2
         else handleFailure res2 ("[5, 3]" :: String))
         `catch` \(e ::ErrorCall) -> handleRuntimeError e
+
+    let final_res = [resAssert1, resAssert2]
+
+    (if final_res
+        then handleOk
+        else output [])
+        `catch` \(e ::ErrorCall) -> handleRuntimeError e
+
