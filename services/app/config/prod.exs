d39431fc67580abe2c019ada0e1ba8e522ac2a8f
vtm9 <vtmilyakov@yandex.ru>
2019-09-04 15:15:23 +0300

    Add new envs

--- services/app/config/prod.exs
+++ services/app/config/prod.exs
@@ -25,9 +25,8 @@ config :phoenix, :serve_endpoints, true
 config :codebattle, Codebattle.Repo,
   adapter: Ecto.Adapters.Postgres,
   database: "codebattle",
-  pool_size: 15
-
-config :codebattle, Codebattle.Repo,
+  ssl: true,
+  port: System.get_env("CODEBATTLE_DB_PORT"),
   username: System.get_env("CODEBATTLE_DB_USERNAME"),
   password: System.get_env("CODEBATTLE_DB_PASSWORD"),
   database: System.get_env("CODEBATTLE_DB_NAME"),
@@ -45,44 +44,3 @@ config :logger, :info,
   level: :info
 
 config :codebattle, Codebattle.Bot, timeout: 1000
-
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-# import_config "prod.secret.exs"
-
-# ## SSL Support
-#
-# To get SSL working, you will need to add the `https` key
-# to the previous section and set your `:url` port to 443:
-#
-#     config :codebattle, Codebattle.Endpoint,
-#       ...
-#       url: [host: "example.com", port: 443],
-#       https: [port: 443,
-#               keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-#               certfile: System.get_env("SOME_APP_SSL_CERT_PATH")]
-#
-# Where those two env variables return an absolute path to
-# the key and cert in disk or a relative path inside priv,
-# for example "priv/ssl/server.key".
-#
-# We also recommend setting `force_ssl`, ensuring no data is
-# ever sent via http, always redirecting to https:
-#
-#     config :codebattle, Codebattle.Endpoint,
-#       force_ssl: [hsts: true]
-#
-# Check `Plug.SSL` for all available options in `force_ssl`.
-
-# ## Using releases
-#
-# If you are doing OTP releases, you need to instruct Phoenix
-# to start the server for all endpoints:
-#
-#     config :phoenix, :serve_endpoints, true
-#
-# Alternatively, you can configure exactly which server to
-# start per endpoint:
-#
-#     config :codebattle, Codebattle.Endpoint, server: true
-#
