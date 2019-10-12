ea75cb820dce307230ab9e4b2affdf4950623bca
igor-i <inkovskiy@gmail.com>
2019-10-12 18:12:02 +0300

    auto format by mix format

--- services/app/lib/codebattle/generators/checker_generator.ex
+++ services/app/lib/codebattle/generators/checker_generator.ex
@@ -295,7 +295,11 @@ defmodule Codebattle.Generators.CheckerGenerator do
   defp filter_empty_items(items), do: items |> Enum.filter(&(&1 != ""))
 
   defp double_backslashes(string),
-    do: string |> String.replace("\\", "\\\\") |> String.replace("\n", "\\n") |> String.replace("\t", "\\t")
+    do:
+      string
+      |> String.replace("\\", "\\\\")
+      |> String.replace("\n", "\\n")
+      |> String.replace("\t", "\\t")
 
   defp put_types(binding, %{slug: slug} = meta, task) when slug in @langs_need_types do
     binding
