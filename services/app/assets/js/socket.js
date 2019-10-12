9e7742d2aec7e764ad9a57cf76de9f3e20b4c91c
vtm9 <vtmilyakov@yandex.ru>
2019-10-12 19:29:32 +0300

    Update js deps

--- services/app/assets/js/socket.js
+++ services/app/assets/js/socket.js
@@ -2,14 +2,9 @@ import { Socket } from 'phoenix';
 import Gon from 'gon';
 
 const socket = new Socket('/ws', {
-  params: { token: Gon.getAsset('user_token') },
-  logger: (kind, msg, data) => { console.log(`${kind}: ${msg}`, data); },
+  params: { token: Gon.getAsset('user_token') }
 });
 
 socket.connect();
-
-socket.onOpen((ev) => console.log('SOCKET OPEN', ev));
-socket.onError((ev) => console.log('SOCKET ERROR', ev));
-socket.onClose((ev) => console.log('SOCKET CLOSE', ev));
 
 export default socket;
