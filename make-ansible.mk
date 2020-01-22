5f066bc01c0e119033018e4a7ce53114b662f317
Eldar Babaev <el.glagius@gmail.com>
2020-01-22 23:51:57 +0700

    Fixes for prodaction environment

--- make-ansible.mk
+++ make-ansible.mk
@@ -5,7 +5,8 @@ setup-env:
 	docker run  -v $(CURDIR):/app -w /app williamyeh/ansible:ubuntu18.04 ansible-playbook ansible/development.yml -i ansible/development -vv
 
 ansible-edit-secrets:
-	ansible-vault edit ansible/production/group_vars/all/vault.yml
+	ansible-vault edit --vault-password-file tmp/ansible-vault-password ansible/production/group_vars/all/vault.yml
+
 
 ansible-vault-edit-production:
 	docker run -v $(CURDIR):/app -it -w /app ansible ansible-vault --vault-password-file tmp/ansible-vault-password edit ansible/production/group_vars/all/vault.yml
