750b9d8fd49c683f692755d3e0f41534567a8bfe
ReDBrother <bataevdmitriy@gmail.com>
2020-02-05 08:01:19 +0300

    save chat actions to playbook

--- services/app/lib/codebattle_web/channels/chat_channel.ex
+++ services/app/lib/codebattle_web/channels/chat_channel.ex
@@ -4,26 +4,47 @@ defmodule CodebattleWeb.ChatChannel do
 
   require Logger
 
-  alias Codebattle.Chat.Server
+  alias Codebattle.Chat
+  alias Codebattle.GameProcess
 
   def join("chat:" <> chat_id, _payload, socket) do
     send(self(), :after_join)
-    {:ok, users} = Server.join_chat(chat_id, socket.assigns.current_user)
-    msgs = Server.get_msgs(chat_id)
+    {:ok, users} = Chat.Server.join_chat(chat_id, socket.assigns.current_user)
+
+    GameProcess.Server.update_playbook(
+      chat_id,
+      :join_chat,
+      %{
+        id: socket.assigns.current_user.id,
+        name: socket.assigns.current_user.name
+      }
+    )
+
+    msgs = Chat.Server.get_msgs(chat_id)
 
     {:ok, %{users: users, messages: msgs}, socket}
   end
 
   def handle_info(:after_join, socket) do
     chat_id = get_chat_id(socket)
-    users = Server.get_users(chat_id)
+    users = Chat.Server.get_users(chat_id)
     broadcast_from!(socket, "user:joined", %{users: users})
     {:noreply, socket}
   end
 
   def terminate(_reason, socket) do
     chat_id = get_chat_id(socket)
-    {:ok, users} = Server.leave_chat(chat_id, socket.assigns.current_user)
+    {:ok, users} = Chat.Server.leave_chat(chat_id, socket.assigns.current_user)
+
+    GameProcess.Server.update_playbook(
+      chat_id,
+      :leave_chat,
+      %{
+        id: socket.assigns.current_user.id,
+        name: socket.assigns.current_user.name
+      }
+    )
+
     broadcast_from!(socket, "user:left", %{users: users})
     {:noreply, socket}
   end
@@ -32,7 +53,18 @@ defmodule CodebattleWeb.ChatChannel do
     %{"message" => message} = payload
     user = socket.assigns.current_user.name
     chat_id = get_chat_id(socket)
-    Server.add_msg(chat_id, user, message)
+
+    Chat.Server.add_msg(chat_id, user, message)
+
+    GameProcess.Server.update_playbook(
+      chat_id,
+      :add_chat_message,
+      %{
+        id: socket.assigns.current_user.id,
+        name: user,
+        message: message
+      }
+    )
 
     broadcast!(socket, "new:message", %{user: user, message: message})
     {:noreply, socket}
