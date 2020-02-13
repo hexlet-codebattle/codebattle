68f401b76e6a40b5c18b1bf13e297e7edfaaa45f
manov <imanovplus@gmail.com>
2020-02-13 19:30:38 +0300

    add pie chart without connection with db

--- services/app/assets/js/widgets/containers/LangPieChart.jsx
+++ services/app/assets/js/widgets/containers/LangPieChart.jsx
@@ -1,57 +1,40 @@
 import React from 'react';
-import axios from 'axios';
-import PieChart from 'react-minimal-pie-chart';
-import Loading from '../components/Loading';
-
-const dataMock = [
-  { title: 'One', value: 10, color: '#E38627' },
-  { title: 'Two', value: 15, color: '#C13C37' },
-  { title: 'Three', value: 20, color: '#6A2135' },
-];
+import ReactApexChart from 'react-apexcharts'
 
 class LangPieChart extends React.Component {
-  state = { data: null };
+  constructor(props) {
+    super(props);
 
-  componentDidMount() {
-    const userId = window.location.pathname.split('/').pop();
-    axios.get(`/api/v1/${userId}/lang_stats`).then((response) => {
-      this.setState(response.data);
-    });
+    this.state = {
+          
+      series: [44, 55, 13, 43, 22],
+      options: {
+        chart: {
+          width: 380,
+          type: 'pie',
+        },
+        labels: ['Ruby', 'PHP', 'JavaScript', 'Erlang', 'C++'],
+        responsive: [{
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              position: 'bottom'
+            }
+          }
+        }]
+      },
+    
+    
+    };
   }
-
   render() {
-    const { data } = this.state;
-    if (!data) {
-      return <Loading />;
-    }
-
     return (
-      <PieChart
-        animate={false}
-        animationDuration={500}
-        animationEasing="ease-out"
-        cx={50}
-        cy={50}
-        data={dataMock}
-        label
-        labelPosition={50}
-        labelStyle={{
-          fill: '#121212',
-          fontFamily: 'sans-serif',
-          fontSize: '5px',
-        }}
-        lengthAngle={360}
-        lineWidth={100}
-        onClick={undefined}
-        onMouseOut={undefined}
-        onMouseOver={undefined}
-        paddingAngle={0}
-        radius={75}
-        rounded={false}
-        startAngle={0}
-        viewBoxSize={[100, 100]}
-      />
-      // TODO: Add labels table
+<div id="chart">
+<ReactApexChart options={this.state.options} series={this.state.series} type="pie" width={380} />
+</div>
     );
   }
 }
