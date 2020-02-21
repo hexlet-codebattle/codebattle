2a22772cd3760e6d4994bb9b421673cd20d35a2d
Irina Nikolaidi <irin.work42@gmail.com>
2020-02-22 01:06:23 +0300

    fix eslint errors

--- services/app/assets/js/widgets/utils/makeCreateGameUrl.js
+++ services/app/assets/js/widgets/utils/makeCreateGameUrl.js
@@ -1,20 +1,22 @@
 import qs from 'qs';
 
-export default ({gameLevel, gameType, timeoutSeconds, gameId}) => {
-    const getDefaultUrl = () => {
-        const queryParamsString = qs.stringify({
-            level: gameLevel,
-            type: gameType,
-            timeout_seconds: timeoutSeconds,
-          });
-          return `/games?${queryParamsString}`;    
-    };
-    const mapTypes = {
-        withBot: () => `/games/${gameId}/join`,
-        withRandomPlayer: getDefaultUrl,
-        withFriend: getDefaultUrl,
-    };
+export default ({
+  gameLevel, gameType, timeoutSeconds, gameId,
+}) => {
+  const getDefaultUrl = () => {
+    const queryParamsString = qs.stringify({
+      level: gameLevel,
+      type: gameType,
+      timeout_seconds: timeoutSeconds,
+    });
+    return `/games?${queryParamsString}`;
+  };
+  const mapTypes = {
+    withBot: () => `/games/${gameId}/join`,
+    withRandomPlayer: getDefaultUrl,
+    withFriend: getDefaultUrl,
+  };
 
-    const getUrl = mapTypes[gameType];
-    return getUrl();
-}
+  const getUrl = mapTypes[gameType];
+  return getUrl();
+};
