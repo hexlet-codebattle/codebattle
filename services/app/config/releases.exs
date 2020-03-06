473e4f5e6fb3da1441e07f79ddc3411d4520b009
vtm9 <vtmilyakov@yandex.ru>
2020-03-06 20:24:27 +0300

    Fix env variables for release

--- services/app/config/releases.exs
+++ services/app/config/releases.exs
@@ -19,9 +19,17 @@ config :codebattle, Codebattle.Repo,
   adapter: Ecto.Adapters.Postgres,
   database: System.get_env("CODEBATTLE_DB_NAME"),
   ssl: true,
-  port: System.get_env("CODEBATTLE_DB_PORT"),
-  username: System.get_env("CODEBATTLE_DB_USERNAME"),
-  password: System.get_env("CODEBATTLE_DB_PASSWORD"),
-  database: System.get_env("CODEBATTLE_DB_NAME"),
-  hostname: System.get_env("CODEBATTLE_DB_HOSTNAME"),
+  port: System.fetch_env!("CODEBATTLE_DB_PORT"),
+  username: System.fetch_env!("CODEBATTLE_DB_USERNAME"),
+  password: System.fetch_env!("CODEBATTLE_DB_PASSWORD"),
+  database: System.fetch_env!("CODEBATTLE_DB_NAME"),
+  hostname: System.fetch_env!("CODEBATTLE_DB_HOSTNAME"),
   pool_size: 7
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: System.fetch_env!("GITHUB_CLIENT_ID"),
+  client_secret: System.fetch_env!("GITHUB_CLIENT_SECRET")
+
+config :one_signal, OneSignal,
+  app_id: System.fetch_env!("ONESIGNAL_APP_ID"),
+  api_key: System.fetch_env!("ONESIGNAL_API_KEY")
