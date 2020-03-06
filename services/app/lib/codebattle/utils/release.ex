f71630bae602702f71302b5b00eb15976580602d
vtm9 <vtmilyakov@yandex.ru>
2020-03-06 18:15:45 +0300

    Fix release migrations

--- services/app/lib/codebattle/utils/release.ex
+++ services/app/lib/codebattle/utils/release.ex
@@ -4,13 +4,10 @@ defmodule Codebattle.Utils.Release do
   @app :codebattle
 
   def migrate do
+    Application.ensure_all_started(:ssl)
     for repo <- repos() do
       {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
     end
-  end
-
-  def rollback(repo, version) do
-    {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
   end
 
   defp repos do
