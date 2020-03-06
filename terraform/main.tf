3dec84c7fc07a027f6f4add412999eda29d13c23
Vagin <laplugin73@gmail.com>
2020-03-06 08:14:41 +0000

    store state in bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
-  provider "digitalocean" {
-    token   = ""
+
+terraform {
+  backend "gcs" {
+    bucket  = "codebattle-terraform-state"
+    prefix  = "production"
+    credentials = "google.key.json"
   }
+}
+
+provider "digitalocean" {
+  token   = ""
+}
