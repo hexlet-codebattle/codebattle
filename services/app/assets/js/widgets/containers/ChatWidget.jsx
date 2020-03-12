89656557206c8fd943195ec9df158d3f059e961c
Victor Zhuravlev <mocambe@mail.ru>
2020-03-12 09:17:01 +0300

    add emoji to chat

--- services/app/assets/js/widgets/containers/ChatWidget.jsx
+++ services/app/assets/js/widgets/containers/ChatWidget.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import _ from 'lodash';
 import { connect } from 'react-redux';
+import { Picker, Emoji } from 'emoji-mart';
 import { fetchState, addMessage } from '../middlewares/Chat';
 import * as selectors from '../selectors';
 import Messages from '../components/Messages';
 import UserName from '../components/UserName';
 import GameStatusCodes from '../config/gameStatusCodes';
+import 'emoji-mart/css/emoji-mart.css';
 
 class ChatWidget extends React.Component {
-  state = { message: '' };
+  state = { message: '', isEmojiPickerVisible: false, isEmojiTooltipVisible: false };
 
   componentDidMount() {
     const { dispatch, isStoredGame } = this.props;
@@ -34,17 +36,52 @@ class ChatWidget extends React.Component {
     }
   };
 
+  toggleEmojiPickerVisibility = e => {
+    e.preventDefault();
+    const { isEmojiPickerVisible, ...rest } = this.state;
+    this.setState({ ...rest, isEmojiPickerVisible: !isEmojiPickerVisible });
+  }
+
+  handleSelect = emoji => {
+    const { message, ...rest } = this.state;
+    this.setState({ ...rest, message: `${message}${emoji.native}`, isEmojiPickerVisible: false });
+  }
+
   renderChatInput() {
-    const { message: typedMessage } = this.state;
+    const { message: typedMessage, isEmojiPickerVisible } = this.state;
 
     return (
       <form className="p-2 input-group input-group-sm position-absolute" style={{ bottom: 0 }} onSubmit={this.handleSubmit}>
         <input
-          className="form-control border-secondary"
+          className="form-control border-secondary relative"
           placeholder="Type message here..."
           value={typedMessage}
           onChange={this.handleChange}
         />
+        <button
+          type="button"
+          className="btn btn-link position-absolute"
+          style={{ right: '50px', zIndex: 10 }}
+          onClick={this.toggleEmojiPickerVisibility}
+        >
+          <Emoji
+            emoji="grinning"
+            set="apple"
+            size={20}
+          />
+        </button>
+        {isEmojiPickerVisible
+          && (
+          <Picker
+            showPreview={false}
+            showSkinTones={false}
+            darkMode={false}
+            include={['recent']}
+            perLine={10}
+            onClick={this.handleSelect}
+            style={{ position: 'absolute', right: '88px', bottom: '10px' }}
+          />
+          )}
         <div className="input-group-append">
           <button className="btn btn-outline-secondary" type="button" onClick={this.handleSubmit}>
             Send
