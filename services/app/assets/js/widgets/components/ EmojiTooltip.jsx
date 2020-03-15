94c3b23cfe0f5540035d6a317e73610111fa5ed4
Victor Zhuravlev <46919813+v1valasvegan@users.noreply.github.com>
2020-03-15 15:52:53 +0300

    Add standard set of emojis to chat (#657)

---
+++ services/app/assets/js/widgets/components/ EmojiTooltip.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import { emojiIndex } from 'emoji-mart';
+import { useHotkeys } from 'react-hotkeys-hook';
+
+const selectStyles = {
+  position: 'absolute',
+  bottom: '39px',
+  display: 'flex',
+  flexDirection: 'column',
+  maxHeight: '100px',
+  maxWidth: '300px',
+  overflow: 'scroll',
+  border: '1px solid #c3c3c3',
+  borderRadius: '5px',
+};
+
+
+export default function EmojiTooltip({ message, handleSelect, hide }) {
+  const lastIndexOfColons = message.lastIndexOf(':');
+  const colons = message.slice(lastIndexOfColons + 1);
+  const emojies = emojiIndex.search(colons);
+
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const increaseIndex = () => {
+    setActiveIndex(prevIndex => {
+      const increment = prevIndex !== emojies.length - 1 ? 1 : -emojies.length + 1;
+      return prevIndex + increment;
+    });
+  };
+
+  const decreaseIndex = () => {
+    setActiveIndex(prevIndex => {
+      const decrement = prevIndex !== 0 ? 1 : -emojies.length + 1;
+      return prevIndex - decrement;
+    });
+  };
+
+  useHotkeys('escape', () => hide(), [], { filter: e => e.target });
+  useHotkeys('enter', () => {
+    handleSelect(colons)(emojies[activeIndex]);
+    hide();
+  }, [], { filter: e => e.target });
+
+  useHotkeys('up', () => decreaseIndex(), [], { filter: e => e.target });
+  useHotkeys('down', () => increaseIndex(), [], { filter: e => e.target });
+
+  const onChange = e => {
+    const [currentIndex] = e.target.value;
+    setActiveIndex(currentIndex);
+  };
+
+
+  if (emojies?.length === 0) {
+    return null;
+  }
+
+  return (
+    <select multiple value={[activeIndex]} onChange={onChange} style={selectStyles}>
+      {emojies?.map((emoji, i) => (
+        <option
+          onClick={() => handleSelect(colons)(emoji)}
+          key={emoji.id}
+          value={+i}
+          style={{ outline: 'none' }}
+        >
+          {emoji.native}
+          {emoji.colons}
+        </option>
+      ))}
+    </select>
+  );
+}
--- services/app/assets/js/widgets/containers/ChatWidget.jsx
+++ services/app/assets/js/widgets/containers/ChatWidget.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import _ from 'lodash';
 import { connect } from 'react-redux';
+import { Emoji } from 'emoji-mart';
 import { fetchState, addMessage } from '../middlewares/Chat';
 import * as selectors from '../selectors';
 import Messages from '../components/Messages';
 import UserName from '../components/UserName';
+import EmojiPicker from '../components/EmojiPicker';
+import EmojiToolTip from '../components/ EmojiTooltip';
 import GameStatusCodes from '../config/gameStatusCodes';
+import 'emoji-mart/css/emoji-mart.css';
 
 class ChatWidget extends React.Component {
-  state = { message: '' };
+  state = { message: '', isEmojiPickerVisible: false, isEmojiTooltipVisible: false };
 
   componentDidMount() {
     const { dispatch, isStoredGame } = this.props;
@@ -17,8 +21,9 @@ class ChatWidget extends React.Component {
     }
   }
 
-  handleChange = e => {
-    this.setState({ message: e.target.value });
+  handleChange = ({ target: { value } }) => {
+    const isEmojiTooltipVisible = /.*:[a-zA-Z]{1,}([^ ])+$/.test(value);
+    this.setState({ message: value, isEmojiTooltipVisible });
   };
 
   handleSubmit = e => {
@@ -34,17 +39,71 @@ class ChatWidget extends React.Component {
     }
   };
 
+  toggleEmojiPickerVisibility = () => {
+    const { isEmojiPickerVisible } = this.state;
+    this.setState({ isEmojiPickerVisible: !isEmojiPickerVisible });
+  };
+
+  hideEmojiPicker = () => {
+    this.setState({ isEmojiPickerVisible: false });
+  };
+
+  handleSelectEmodji = (colons = null) => emoji => {
+    const { message } = this.state;
+    const messageWithoutColons = colons ? message.slice(0, -colons.length - 2) : message;
+    this.setState({
+      message: `${messageWithoutColons}${emoji.native}`,
+      isEmojiPickerVisible: false,
+    });
+  };
+
+  handleInputKeydown = e => {
+    const { isEmojiTooltipVisible } = this.state;
+    if (e.key === 'Enter' && isEmojiTooltipVisible) {
+      e.preventDefault();
+    }
+  }
+
+  hideEmojiTooltip = () => this.setState({ isEmojiTooltipVisible: false });
+
   renderChatInput() {
-    const { message: typedMessage } = this.state;
+    const { message, isEmojiPickerVisible, isEmojiTooltipVisible } = this.state;
 
     return (
-      <form className="p-2 input-group input-group-sm position-absolute" style={{ bottom: 0 }} onSubmit={this.handleSubmit}>
+      <form
+        className="p-2 input-group input-group-sm position-absolute"
+        style={{ bottom: 0 }}
+        onSubmit={this.handleSubmit}
+      >
         <input
-          className="form-control border-secondary"
+          className="form-control border-secondary relative"
           placeholder="Type message here..."
-          value={typedMessage}
+          value={message}
           onChange={this.handleChange}
+          onKeyDown={this.handleInputKeydown}
         />
+        <button
+          type="button"
+          className="btn btn-link position-absolute"
+          style={{ right: '50px', zIndex: 5 }}
+          onClick={this.toggleEmojiPickerVisibility}
+        >
+          <Emoji emoji="grinning" set="apple" size={20} />
+        </button>
+        {isEmojiTooltipVisible && (
+          <EmojiToolTip
+            message={message}
+            handleSelect={this.handleSelectEmodji}
+            hide={this.hideEmojiTooltip}
+          />
+        )}
+        {isEmojiPickerVisible && (
+          <EmojiPicker
+            handleSelect={this.handleSelectEmodji}
+            hideEmojiPicker={this.hideEmojiPicker}
+            isShown={isEmojiPickerVisible}
+          />
+        )}
         <div className="input-group-append">
           <button className="btn btn-outline-secondary" type="button" onClick={this.handleSubmit}>
             Send
@@ -61,19 +120,14 @@ class ChatWidget extends React.Component {
     return (
       <div className="d-flex shadow-sm h-100">
         <div className="col-12 col-sm-8 p-0 bg-white rounded-left h-100 position-relative">
-          <Messages
-            messages={messages}
-          />
-          { !isStoredGame && this.renderChatInput() }
+          <Messages messages={messages} />
+          {!isStoredGame && this.renderChatInput()}
         </div>
         <div className="col-4 d-none d-sm-block p-0 border-left bg-white rounded-right">
           <div className="d-flex flex-direction-column flex-wrap justify-content-between">
             <div className="px-3 pt-3 pb-2 w-100">
               <p className="mb-1">{`Online users: ${users.length}`}</p>
-              <div
-                className="overflow-auto"
-                style={{ height: '175px' }}
-              >
+              <div className="overflow-auto" style={{ height: '175px' }}>
                 {listOfUsers.map(user => (
                   <div key={user.id} className="my-2">
                     <UserName user={user} />
@@ -95,7 +149,4 @@ const mapStateToProps = state => ({
   isStoredGame: selectors.gameStatusSelector(state).status === GameStatusCodes.stored,
 });
 
-export default connect(
-  mapStateToProps,
-  null,
-)(ChatWidget);
+export default connect(mapStateToProps, null)(ChatWidget);
