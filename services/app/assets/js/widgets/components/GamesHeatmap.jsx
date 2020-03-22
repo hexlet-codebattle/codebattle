b063dde10f89fc95262d7108e53962ddd62a3956
Vova Afan <grmh9ko@gmail.com>
2020-03-22 23:40:04 +0300

    671-refactor-GamesHeatmap

--- services/app/assets/js/widgets/components/GamesHeatmap.jsx
+++ services/app/assets/js/widgets/components/GamesHeatmap.jsx
@@ -1,62 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CalendarHeatmap from 'react-calendar-heatmap';
 import axios from 'axios';
 import Loading from './Loading';
 
-class GamesHeatmap extends React.Component {
-  static colorScale(count) {
-    if (count >= 5) {
-      return 'color-huge';
-    }
-    if (count >= 3) {
-      return 'color-large';
-    }
-    if (count >= 1) {
-      return 'color-small';
-    }
-    return 'color-empty';
-  }
+const GamesHeatmap = () => {
+  const [activities, setActivities] = useState(null);
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      activities: null,
-    };
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     axios.get('/api/v1/game_activity')
-      .then(response => { this.setState(response.data); });
+      .then(response => { setActivities(response.data.activities); });
+  }, []);
+
+  if (!activities) {
+    return (<Loading />);
   }
-
-
-  render() {
-    const { activities } = this.state;
-    if (!activities) {
-      return (<Loading />);
-    }
-    return (
-      <div className="card rounded">
-        <div className="card-body py-0 my-0">
-          <CalendarHeatmap
-            showWeekdayLabels
-            values={activities}
-            classForValue={value => {
-              if (!value) {
-                return 'color-empty';
-              }
-              return GamesHeatmap.colorScale(value.count);
-            }}
-            titleForValue={value => {
-              if (!value) {
-                return 'No games';
-              }
-              return `${value.count} games on ${value.date}`;
-            }}
-          />
-        </div>
+  return (
+    <div className="card rounded">
+      <div className="card-body py-0 my-0">
+        <CalendarHeatmap
+          showWeekdayLabels
+          values={activities}
+          classForValue={value => {
+            if (!value) {
+              return 'color-empty';
+            }
+            return GamesHeatmap.colorScale(value.count);
+          }}
+          titleForValue={value => {
+            if (!value) {
+              return 'No games';
+            }
+            return `${value.count} games on ${value.date}`;
+          }}
+        />
       </div>
-    );
+    </div>
+  );
+};
+GamesHeatmap.colorScale = count => {
+  if (count >= 5) {
+    return 'color-huge';
   }
-}
+  if (count >= 3) {
+    return 'color-large';
+  }
+  if (count >= 1) {
+    return 'color-small';
+  }
+  return 'color-empty';
+};
 export default GamesHeatmap;
