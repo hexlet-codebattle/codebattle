1d7aa750178ae74c1a583f48733151df9de2bcbc
ReDBrother <bataevdmitriy@gmail.com>
2020-03-28 03:21:54 +0300

    upgrade dart checker

--- services/app/dockers/dart/checker_example.dart
+++ services/app/dockers/dart/checker_example.dart
@@ -1,14 +1,40 @@
 import 'package:test/test.dart';
 import 'package:dart_json/dart_json.dart';
+import 'dart:async';
 
 import 'package:dart/solution_example.dart';
 
 void main() {
   test("Run Asserts", () {
-    var success = true;
+    bool success = true;
+    var asserts = [];
+    var output = "";
 
-    success = assert_solution(solution(1, 2), 3, [1, 2], success);
-    success = assert_solution(solution(6, 10), 16, [6, 10], success);
+    var startTime;
+    var result;
+    var executionTime;
+
+    runZoned(() {
+      startTime = new DateTime.now().millisecondsSinceEpoch;
+      result = solution(1, 2);
+      executionTime = new DateTime.now().millisecondsSinceEpoch - startTime;
+      success = assert_solution(result, 3, [1, 2], output, asserts, executionTime, success);
+      output = "";
+
+      startTime = new DateTime.now().millisecondsSinceEpoch;
+      result = solution(6, 10);
+      executionTime = new DateTime.now().millisecondsSinceEpoch - startTime;
+      success = assert_solution(result, 16, [6, 10], output, asserts, executionTime, success);
+      output = "";
+    }, zoneSpecification: new ZoneSpecification(
+      print: (Zone self, ZoneDelegate parent, Zone zone, String line) {
+        output += line + "\n";
+      }
+    ));
+
+    asserts.forEach(
+      (message) => print_message(message)
+    );
 
     if (success) {
       print_message({'status': 'ok', 'result': '__code-0__'});
@@ -16,12 +42,26 @@ void main() {
   });
 }
 
-bool assert_solution(result, expected, arguments, success) {
+bool assert_solution(result, expected, arguments, output, asserts, executionTime, success) {
   try {
     expect(expected, equals(result));
-    print_message({'status': 'success', 'result': result});
+    asserts.add({
+      'status': 'success',
+      'result': result,
+      'output': output,
+      'expected': expected,
+      'arguments': arguments,
+      'execution_time': executionTime
+    });
   } catch (e) {
-    print_message({'status': 'failure', 'result': result, 'arguments': arguments});
+    asserts.add({
+      'status': 'failure',
+      'result': result,
+      'output': output,
+      'expected': expected,
+      'arguments': arguments,
+      'execution_time': executionTime
+    });
     return false;
   }
 
