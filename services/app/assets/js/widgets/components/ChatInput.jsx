13da18812b48557c35d46b3a05da855d34758b70
Victor Zhuravlev <mocambe@mail.ru>
2020-03-31 20:31:47 +0300

    remove useless first argument from addMessage

--- services/app/assets/js/widgets/components/ChatInput.jsx
+++ services/app/assets/js/widgets/components/ChatInput.jsx
@@ -31,7 +31,7 @@ export default function ChatInput() {
     e.preventDefault();
 
     if (message) {
-      addMessage(null, message);
+      addMessage(message);
       setMessage('');
     }
   };
