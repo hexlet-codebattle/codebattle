8152740a84f21fd01158c7571478aec0a71ddf4d
Vova Afan <grmh9ko@gmail.com>
2020-04-02 18:02:15 +0300

    671-refactor-ChatWidget

--- services/app/assets/js/widgets/containers/ChatWidget.jsx
+++ services/app/assets/js/widgets/containers/ChatWidget.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import _ from 'lodash';
 import { connect } from 'react-redux';
 import { Emoji } from 'emoji-mart';
@@ -11,136 +11,126 @@ import EmojiToolTip from '../components/ EmojiTooltip';
 import GameStatusCodes from '../config/gameStatusCodes';
 import 'emoji-mart/css/emoji-mart.css';
 
-class ChatWidget extends React.Component {
-  state = { message: '', isEmojiPickerVisible: false, isEmojiTooltipVisible: false };
-
-  componentDidMount() {
-    const { dispatch, isStoredGame } = this.props;
+const ChatWidget = props => {
+  const [message, setMessage] = useState('');
+  const [isEmojiPickerVisible, setIsEmojiPickerVisible] = useState(false);
+  const [isEmojiTooltipVisible, setIsEmojiTooltipVisible] = useState(false);
+  useEffect(() => {
+    const { dispatch, isStoredGame } = props;
     if (!isStoredGame) {
       dispatch(fetchState());
     }
-  }
+  }, []);
 
-  handleChange = ({ target: { value } }) => {
-    const isEmojiTooltipVisible = /.*:[a-zA-Z]{1,}([^ ])+$/.test(value);
-    this.setState({ message: value, isEmojiTooltipVisible });
+  const handleChange = ({ target: { value } }) => {
+    const EmojiTooltipVisible = /.*:[a-zA-Z]{1,}([^ ])+$/.test(value);
+    setIsEmojiTooltipVisible(EmojiTooltipVisible);
+    setMessage(value);
   };
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    const { message } = this.state;
     const {
       currentUser: { name },
-    } = this.props;
+    } = props;
 
     if (message) {
       addMessage(name, message);
-      this.setState({ message: '' });
+      setMessage('');
     }
   };
 
-  toggleEmojiPickerVisibility = () => {
-    const { isEmojiPickerVisible } = this.state;
-    this.setState({ isEmojiPickerVisible: !isEmojiPickerVisible });
+  const toggleEmojiPickerVisibility = () => {
+    setIsEmojiPickerVisible(!isEmojiPickerVisible);
   };
 
-  hideEmojiPicker = () => {
-    this.setState({ isEmojiPickerVisible: false });
+  const hideEmojiPicker = () => {
+    setIsEmojiPickerVisible(false);
   };
 
-  handleSelectEmodji = (colons = null) => emoji => {
-    const { message } = this.state;
+  const handleSelectEmodji = (colons = null) => emoji => {
     const messageWithoutColons = colons ? message.slice(0, -colons.length - 2) : message;
-    this.setState({
-      message: `${messageWithoutColons}${emoji.native}`,
-      isEmojiPickerVisible: false,
-    });
+    setIsEmojiPickerVisible(false);
+    setMessage(`${messageWithoutColons}${emoji.native}`);
   };
 
-  handleInputKeydown = e => {
-    const { isEmojiTooltipVisible } = this.state;
+  const handleInputKeydown = e => {
     if (e.key === 'Enter' && isEmojiTooltipVisible) {
       e.preventDefault();
     }
-  }
+  };
 
-  hideEmojiTooltip = () => this.setState({ isEmojiTooltipVisible: false });
+  const hideEmojiTooltip = () => setIsEmojiTooltipVisible(false);
 
-  renderChatInput() {
-    const { message, isEmojiPickerVisible, isEmojiTooltipVisible } = this.state;
-
-    return (
-      <form
-        className="p-2 input-group input-group-sm position-absolute"
-        style={{ bottom: 0 }}
-        onSubmit={this.handleSubmit}
+  const renderChatInput = () => (
+    <form
+      className="p-2 input-group input-group-sm position-absolute"
+      style={{ bottom: 0 }}
+      onSubmit={handleSubmit}
+    >
+      <input
+        className="form-control border-secondary relative"
+        placeholder="Type message here..."
+        value={message}
+        onChange={handleChange}
+        onKeyDown={handleInputKeydown}
+      />
+      <button
+        type="button"
+        className="btn btn-link position-absolute"
+        style={{ right: '50px', zIndex: 5 }}
+        onClick={toggleEmojiPickerVisibility}
       >
-        <input
-          className="form-control border-secondary relative"
-          placeholder="Type message here..."
-          value={message}
-          onChange={this.handleChange}
-          onKeyDown={this.handleInputKeydown}
-        />
-        <button
-          type="button"
-          className="btn btn-link position-absolute"
-          style={{ right: '50px', zIndex: 5 }}
-          onClick={this.toggleEmojiPickerVisibility}
-        >
-          <Emoji emoji="grinning" set="apple" size={20} />
+        <Emoji emoji="grinning" set="apple" size={20} />
+      </button>
+      {isEmojiTooltipVisible && (
+      <EmojiToolTip
+        message={message}
+        handleSelect={handleSelectEmodji}
+        hide={hideEmojiTooltip}
+      />
+      )}
+      {isEmojiPickerVisible && (
+      <EmojiPicker
+        handleSelect={handleSelectEmodji}
+        hideEmojiPicker={hideEmojiPicker}
+        isShown={isEmojiPickerVisible}
+      />
+      )}
+      <div className="input-group-append">
+        <button className="btn btn-outline-secondary" type="button" onClick={handleSubmit}>
+          Send
         </button>
-        {isEmojiTooltipVisible && (
-          <EmojiToolTip
-            message={message}
-            handleSelect={this.handleSelectEmodji}
-            hide={this.hideEmojiTooltip}
-          />
-        )}
-        {isEmojiPickerVisible && (
-          <EmojiPicker
-            handleSelect={this.handleSelectEmodji}
-            hideEmojiPicker={this.hideEmojiPicker}
-            isShown={isEmojiPickerVisible}
-          />
-        )}
-        <div className="input-group-append">
-          <button className="btn btn-outline-secondary" type="button" onClick={this.handleSubmit}>
-            Send
-          </button>
-        </div>
-      </form>
-    );
-  }
+      </div>
+    </form>
+  );
+  const { messages, users, isStoredGame } = props;
+  const listOfUsers = _.uniqBy(users, 'id');
 
-  render() {
-    const { messages, users, isStoredGame } = this.props;
-    const listOfUsers = _.uniqBy(users, 'id');
-
-    return (
-      <div className="d-flex shadow-sm h-100">
-        <div className="col-12 col-sm-8 p-0 bg-white rounded-left h-100 position-relative">
-          <Messages messages={messages} />
-          {!isStoredGame && this.renderChatInput()}
-        </div>
-        <div className="col-4 d-none d-sm-block p-0 border-left bg-white rounded-right">
-          <div className="d-flex flex-direction-column flex-wrap justify-content-between">
-            <div className="px-3 pt-3 pb-2 w-100">
-              <p className="mb-1">{`Online users: ${users.length}`}</p>
-              <div className="overflow-auto" style={{ height: '175px' }}>
-                {listOfUsers.map(user => (
-                  <div key={user.id} className="my-2">
-                    <UserName user={user} />
-                  </div>
-                ))}
-              </div>
+  return (
+    <div className="d-flex shadow-sm h-100">
+      <div className="col-12 col-sm-8 p-0 bg-white rounded-left h-100 position-relative">
+        <Messages messages={messages} />
+        {!isStoredGame && renderChatInput()}
+      </div>
+      <div className="col-4 d-none d-sm-block p-0 border-left bg-white rounded-right">
+        <div className="d-flex flex-direction-column flex-wrap justify-content-between">
+          <div className="px-3 pt-3 pb-2 w-100">
+            <p className="mb-1">{`Online users: ${users.length}`}</p>
+            <div className="overflow-auto" style={{ height: '175px' }}>
+              {listOfUsers.map(user => (
+                <div key={user.id} className="my-2">
+                  <UserName user={user} />
+                </div>
+              ))}
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
 
 const mapStateToProps = state => ({
   users: selectors.chatUsersSelector(state),
