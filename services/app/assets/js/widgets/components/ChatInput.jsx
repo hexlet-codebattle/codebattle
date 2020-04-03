fe64cc2a94ed40301b1f59cbbdc8636fe27eacc7
Victor Zhuravlev <mocambe@mail.ru>
2020-04-03 22:08:10 +0300

    refactor, remove unnecessary prop

--- services/app/assets/js/widgets/components/ChatInput.jsx
+++ services/app/assets/js/widgets/components/ChatInput.jsx
@@ -9,18 +9,18 @@ const trimColons = message => message.slice(0, message.lastIndexOf(':'));
 
 const getColons = message => message.slice(message.lastIndexOf(':') + 1);
 
+const getTooltipVisibility = msg => {
+  const endsWithEmojiCodeRegex = /.*:[a-zA-Z]{1,}([^ ])+$/;
+  if (!endsWithEmojiCodeRegex.test(msg)) return false;
+  const colons = getColons(msg);
+  return !_.isEmpty(emojiIndex.search(colons));
+};
+
 export default function ChatInput() {
   const [isPickerVisible, setPickerVisibility] = useState(false);
   const [isTooltipVisible, setTooltipVisibility] = useState(false);
   const [message, setMessage] = useState('');
   const inputRef = useRef(null);
-
-  const getTooltipVisibility = msg => {
-    const endsWithEmojiCodeRegex = /.*:[a-zA-Z]{1,}([^ ])+$/;
-    if (!endsWithEmojiCodeRegex.test(msg)) return false;
-    const colons = getColons(msg);
-    return !_.isEmpty(emojiIndex.search(colons));
-  };
 
   const handleChange = ({ target: { value } }) => {
     setTooltipVisibility(getTooltipVisibility(value));
@@ -91,12 +91,11 @@ export default function ChatInput() {
           hide={hideTooltip}
         />
       )}
-      {isPickerVisible && (
-        <EmojiPicker
-          handleSelect={handleSelectEmodji}
-          hide={hidePicker}
-          isShown={isPickerVisible}
-        />
+      { isPickerVisible && (
+      <EmojiPicker
+        handleSelect={handleSelectEmodji}
+        hide={hidePicker}
+      />
       )}
       <div className="input-group-append">
         <button className="btn btn-outline-secondary" type="button" onClick={handleSubmit}>
