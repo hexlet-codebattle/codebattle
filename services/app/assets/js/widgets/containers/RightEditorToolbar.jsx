3e2d756b2b68216ba0885a1063f3ae0d5da33945
Vova Afan <grmh9ko@gmail.com>
2020-04-04 11:38:01 +0300

    refactor RightEditorToolbar

--- services/app/assets/js/widgets/containers/RightEditorToolbar.jsx
+++ services/app/assets/js/widgets/containers/RightEditorToolbar.jsx
@@ -1,124 +1,86 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import _ from 'lodash';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import GameStatusCodes from '../config/gameStatusCodes';
 import * as selectors from '../selectors';
-import {
-  checkGameResult,
-  changeCurrentLangAndSetTemplate,
-  compressEditorHeight,
-  expandEditorHeight,
-} from '../middlewares/Game';
+import { compressEditorHeight, expandEditorHeight } from '../middlewares/Game';
 import LanguagePicker from '../components/LanguagePicker';
 import UserInfo from './UserInfo';
 import GameResultIcon from '../components/GameResultIcon';
 
-class RightEditorToolbar extends Component {
-  static defaultProps = {
-    status: GameStatusCodes.initial,
-    title: '',
-    users: {},
-    onlineUsers: [],
-  };
+const renderEditorHeightButtons = (compressEditor, expandEditor, userId) => (
+  <div className="btn-group btn-group-sm mr-2" role="group" aria-label="Editor height">
+    <button
+      type="button"
+      className="btn btn-sm btn-light border rounded"
+      onClick={() => compressEditor(userId)}
+    >
+      <i className="fas fa-compress-arrows-alt" aria-hidden="true" />
+    </button>
+    <button
+      type="button"
+      className="btn btn-sm btn-light border rounded ml-2"
+      onClick={() => expandEditor(userId)}
+    >
+      <i className="fas fa-expand-arrows-alt" aria-hidden="true" />
+    </button>
+  </div>
+);
 
-  renderNameplate = (player = {}, onlineUsers) => {
-    const isOnline = _.find(onlineUsers, { id: player.id });
+const renderNameplate = (player = {}, onlineUsers) => {
+  const isOnline = _.find(onlineUsers, { id: player.id });
 
-    return (
-      <div className="d-flex align-items-center">
-        <UserInfo user={player} />
-        <div>
-          {isOnline ? (
-            <FontAwesomeIcon icon="snowman" className="text-success ml-2" />
-          ) : (
-            <FontAwesomeIcon icon="skull-crossbones" className="text-secondary ml-2" />
-          )}
-        </div>
+  return (
+    <div className="d-flex align-items-center">
+      <UserInfo user={player} />
+      <div>
+        {isOnline ? (
+          <FontAwesomeIcon icon="snowman" className="text-success ml-2" />
+        ) : (
+          <FontAwesomeIcon icon="skull-crossbones" className="text-secondary ml-2" />
+        )}
       </div>
-    );
-  };
-
-  renderEditorHeightButtons = (compressEditor, expandEditor, userId) => (
-    <div className="btn-group btn-group-sm mr-2" role="group" aria-label="Editor height">
-      <button
-        type="button"
-        className="btn btn-sm btn-light border rounded"
-        onClick={() => compressEditor(userId)}
-      >
-        <i className="fas fa-compress-arrows-alt" aria-hidden="true" />
-      </button>
-      <button
-        type="button"
-        className="btn btn-sm btn-light border rounded ml-2"
-        onClick={() => expandEditor(userId)}
-      >
-        <i className="fas fa-expand-arrows-alt" aria-hidden="true" />
-      </button>
     </div>
   );
+};
 
-  render() {
-    const {
-      rightEditorLangSlug,
-      rightUserId,
-      leftUserId,
-      onlineUsers,
-      players,
-      compressEditor,
-      expandEditor,
-      languages,
-    } = this.props;
+const RightEditorToolbar = () => {
+  const rightUserId = useSelector(state => _.get(selectors.rightEditorSelector(state), ['userId'], null));
+  const leftUserId = useSelector(state => _.get(selectors.leftEditorSelector(state), ['userId'], null));
+  const languages = useSelector(state => selectors.editorLangsSelector(state));
+  const onlineUsers = useSelector(state => selectors.chatUsersSelector(state));
+  const rightEditorLangSlug = useSelector(state => selectors.userLangSelector(rightUserId)(state));
+  const players = useSelector(state => selectors.gamePlayersSelector(state));
 
-    if (rightEditorLangSlug === null) {
-      return null;
-    }
+  const dispatch = useDispatch();
+  const compressEditor = userId => dispatch(compressEditorHeight(userId));
+  const expandEditor = userId => dispatch(expandEditorHeight(userId));
 
-    return (
-      <div
-        className="py-2 px-3 btn-toolbar justify-content-between align-items-center"
-        role="toolbar"
-      >
-        <GameResultIcon
-          className="mr-2"
-          resultUser1={_.get(players, [[rightUserId], 'gameResult'])}
-          resultUser2={_.get(players, [[leftUserId], 'gameResult'])}
+  if (rightEditorLangSlug === null) { return null; }
+
+  return (
+    <div
+      className="py-2 px-3 btn-toolbar justify-content-between align-items-center"
+      role="toolbar"
+    >
+      <GameResultIcon
+        className="mr-2"
+        resultUser1={_.get(players, [[rightUserId], 'gameResult'])}
+        resultUser2={_.get(players, [[leftUserId], 'gameResult'])}
+      />
+      {renderNameplate(players[rightUserId], onlineUsers)}
+      <div className="ml-auto btn-group" role="group" aria-label="Editor settings">
+        {renderEditorHeightButtons(compressEditor, expandEditor, rightUserId)}
+        <LanguagePicker
+          languages={languages}
+          currentLangSlug={rightEditorLangSlug}
+          onChange={_.noop}
+          disabled
         />
-        {this.renderNameplate(players[rightUserId], onlineUsers)}
-        <div className="ml-auto btn-group" role="group" aria-label="Editor settings">
-          {this.renderEditorHeightButtons(compressEditor, expandEditor, rightUserId)}
-          <LanguagePicker
-            languages={languages}
-            currentLangSlug={rightEditorLangSlug}
-            onChange={_.noop}
-            disabled
-          />
-        </div>
       </div>
-    );
-  }
-}
-
-const mapStateToProps = state => {
-  const rightUserId = _.get(selectors.rightEditorSelector(state), ['userId'], null);
-  const leftUserId = _.get(selectors.leftEditorSelector(state), ['userId'], null);
-
-  return {
-    rightUserId,
-    leftUserId,
-    languages: selectors.editorLangsSelector(state),
-    onlineUsers: selectors.chatUsersSelector(state),
-    rightEditorLangSlug: selectors.userLangSelector(rightUserId)(state),
-    gameStatus: selectors.gameStatusSelector(state),
-    players: selectors.gamePlayersSelector(state),
-  };
+    </div>
+  );
 };
 
-const mapDispatchToProps = {
-  checkResult: checkGameResult,
-  setLang: changeCurrentLangAndSetTemplate,
-  compressEditor: compressEditorHeight,
-  expandEditor: expandEditorHeight,
-};
 
-export default connect(mapStateToProps, mapDispatchToProps)(RightEditorToolbar);
+export default RightEditorToolbar;
