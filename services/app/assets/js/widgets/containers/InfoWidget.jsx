f07a9049b0340e2c2774bd515d8c962c6769ee6d
Vova Afan <grmh9ko@gmail.com>
2020-04-05 18:41:57 +0300

    refactor-InfoWidget

--- services/app/assets/js/widgets/containers/InfoWidget.jsx
+++ services/app/assets/js/widgets/containers/InfoWidget.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import ChatWidget from './ChatWidget';
 import Task from '../components/Task';
-import * as selectors from '../selectors';
+import { gameTaskSelector, gameStatusSelector } from '../selectors';
 
-const InfoWidget = props => {
-  const {
-    taskText, gameStatusName, timeoutSeconds, startsAt,
-  } = props;
-
+const InfoWidget = () => {
+  const taskText = useSelector(state => gameTaskSelector(state));
+  const startsAt = useSelector(state => gameStatusSelector(state).startsAt);
+  const timeoutSeconds = useSelector(state => gameStatusSelector(state).timeoutSeconds);
+  const gameStatusName = useSelector(state => gameStatusSelector(state).status);
   return (
     <div className="row no-gutters cb-info-widget">
       <div className="col-12 col-lg-6 p-1 h-100">
@@ -26,15 +26,4 @@ const InfoWidget = props => {
   );
 };
 
-
-const mapStateToProps = state => {
-  const gameStatus = selectors.gameStatusSelector(state);
-  return {
-    taskText: selectors.gameTaskSelector(state),
-    startsAt: gameStatus.startsAt,
-    timeoutSeconds: gameStatus.timeoutSeconds,
-    gameStatusName: gameStatus.status,
-  };
-};
-
-export default connect(mapStateToProps)(InfoWidget);
+export default InfoWidget;
