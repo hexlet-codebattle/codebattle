04a78f91f588ad19529b3fcaddf7e8efb4d13224
ReDBrother <bataevdmitriy@gmail.com>
2020-04-15 21:04:16 +0300

    add asserts jsons to checker_status

--- services/app/lib/codebattle/code_check/check_result.ex
+++ services/app/lib/codebattle/code_check/check_result.ex
@@ -2,12 +2,14 @@ defmodule Codebattle.CodeCheck.CheckResult do
   @moduledoc false
 
   # statuses: :initial, :ok, :failure, :error
-  @derive {Poison.Encoder, only: [:status, :output, :result, :asserts_count, :success_count]}
+  @derive {Poison.Encoder,
+           only: [:status, :output, :result, :asserts, :asserts_count, :success_count]}
 
   defstruct success_count: 0,
             asserts_count: 0,
             status: :initial,
             result: "{}",
+            asserts: [],
             output: ""
 
   def new, do: %__MODULE__{}
