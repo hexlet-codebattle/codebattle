aeeaddafefd0530278b4042249be237abfc8cab2
plugin73 <laplugin73@gmail.com>
2020-05-06 12:02:30 +0000

    new cluster

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -10,3 +10,17 @@ resource "digitalocean_kubernetes_cluster" "codebattle" {
     node_count = 1
   }
 }
+
+
+resource "digitalocean_kubernetes_cluster" "codebattle-2" {
+  version = "1.16.6-do.2"
+
+  name = "codebattle-2"
+  region = "fra1"
+
+  node_pool {
+    name       = "codebattle-node-pool"
+    size       = "c-2"
+    node_count = 1
+  }
+}
