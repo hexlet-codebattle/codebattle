22461bb26dfc29290ee2b03312349dcdc3c777dc
ReDBrother <bataevdmitriy@gmail.com>
2020-05-30 21:30:23 +0300

    update UserInfo [remove popover and add guest info]

--- services/app/assets/js/widgets/components/User/UserName.jsx
+++ services/app/assets/js/widgets/components/User/UserName.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
 import _ from 'lodash';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import i18n from '../../../i18n';
 import LanguageIcon from '../LanguageIcon';
 import { loadUser } from '../../middlewares/Users';
-import { getUsersInfo } from '../../selectors';
+import { usersInfoSelector, currentUserIdSelector } from '../../selectors';
 
 const isValidUserInfo = user => (
-  Boolean(user.githubId && user.rating)
+  Boolean(user.id === 0 || user.rating)
 );
 
-const UserName = ({ user, users, dispatch }) => {
+const getName = ({ id, name }, isCurrentUser) => {
+  if (id < 0) {
+    return i18n.t('%{name} (bot)', { name });
+  }
+
+  return isCurrentUser ? i18n.t('%{name} (you)', { name }) : name;
+};
+
+
+const displayDiff = num => {
+  if (num < 0) {
+    return <small className="text-danger">{` ${num}`}</small>;
+  }
+  return <small className="text-success">{` +${num}`}</small>;
+};
+
+const UserName = ({ user }) => {
+  const users = useSelector(usersInfoSelector);
+  const isCurrentUser = useSelector(state => currentUserIdSelector(state) === user.id);
+  const dispatch = useDispatch();
+
   const userInfo = isValidUserInfo(user) ? user : users[user.id];
 
   if (!userInfo) {
@@ -18,38 +39,22 @@ const UserName = ({ user, users, dispatch }) => {
   }
 
   const {
-    id, name, githubId, lang, rating, ratingDiff,
+    id, lang, rating, ratingDiff,
   } = userInfo;
 
   const anonymousUser = (
     <span className="text-secondary">
-      <span className="border rounded align-middle text-center p-1 mr-1">
-        <i className="fa fa-lg fa-user-secret" aria-hidden="true" />
-      </span>
-      {name}
+      {getName(userInfo, isCurrentUser)}
     </span>
   );
-
-  const displayDiff = num => {
-    if (num < 0) {
-      return <small className="text-danger">{` ${num}`}</small>;
-    }
-    return <small className="text-success">{` +${num}`}</small>;
-  };
-  const githubUser = (
+  const loggedUser = (
     <span className="d-flex align-items-center">
       <a
         href={`/users/${id}`}
-        key={githubId}
+        key={id}
         className="d-flex align-items-center mr-1 text-truncate"
       >
-        <img
-          className="attachment rounded border mr-1"
-          alt={name}
-          src={`https://avatars0.githubusercontent.com/u/${githubId}`}
-          width="25px"
-        />
-        <span className="text-truncate">{name}</span>
+        <span className="text-truncate">{getName(userInfo, isCurrentUser)}</span>
       </a>
       <LanguageIcon lang={lang} />
       <small>
@@ -63,13 +68,9 @@ const UserName = ({ user, users, dispatch }) => {
     <div
       className="d-inline align-middle"
     >
-      {id === 'anonymous' ? anonymousUser : githubUser}
+      {id === 0 ? anonymousUser : loggedUser}
     </div>
   );
 };
 
-const mapStateToProps = state => ({
-  users: getUsersInfo(state),
-});
-
-export default connect(mapStateToProps)(UserName);
+export default UserName;
--- services/app/assets/js/widgets/containers/UserInfo.jsx
+++ services/app/assets/js/widgets/containers/UserInfo.jsx
@@ -1,45 +1,50 @@
 // @ts-check
-import { useSelector, useDispatch } from 'react-redux';
+// import { useSelector, useDispatch } from 'react-redux';
 import React from 'react';
-import { OverlayTrigger, Popover } from 'react-bootstrap';
-import cn from 'classnames';
+// import { OverlayTrigger, Popover } from 'react-bootstrap';
+// import cn from 'classnames';
 import UserName from '../components/User/UserName';
-import UserStats from '../components/User/UserStats';
-import { getUsersStats } from '../selectors';
-import { loadUserStats } from '../middlewares/Users';
+// import UserStats from '../components/User/UserStats';
+// import { getUsersStats } from '../selectors';
+// import { loadUserStats } from '../middlewares/Users';
 
-const UserInfo = ({ user }) => {
-  const usersStats = useSelector(state => getUsersStats(state));
-  const userStats = usersStats[user.id];
-  const dispatch = useDispatch();
-  const popoverID = `popover-userInfo-${user.id}`;
+// const UserInfo = ({ user }) => {
+//   const usersStats = useSelector(state => getUsersStats(state));
+//   const userStats = usersStats[user.id];
+//   const dispatch = useDispatch();
+//   const popoverID = `popover-userInfo-${user.id}`;
 
-  const statsPopover = ({ show, ...rest }) => (
-    <Popover className={cn({ 'd-none': !userStats })} id={popoverID} {...rest}>
-      <Popover.Title as="h3">{user.name}</Popover.Title>
-      {userStats && (
-        <Popover.Content>
-          <UserStats data={userStats} />
-        </Popover.Content>
-      )}
-    </Popover>
-  );
+//   const statsPopover = ({ show, ...rest }) => (
+//     <Popover className={cn({ 'd-none': !userStats })} id={popoverID} {...rest}>
+//       <Popover.Title as="h3">{user.name}</Popover.Title>
+//       {userStats && (
+//         <Popover.Content>
+//           <UserStats data={userStats} />
+//         </Popover.Content>
+//       )}
+//     </Popover>
+//   );
 
-  const onEnter = () => !userStats && dispatch(loadUserStats)(user);
+//   const onEnter = () => !userStats && dispatch(loadUserStats)(user);
 
-  return (
-    <OverlayTrigger
-      trigger={['hover', 'focus']}
-      placement="left"
-      overlay={statsPopover}
-      onEnter={onEnter}
-    >
-      <span>
-        <UserName user={user} />
-      </span>
-    </OverlayTrigger>
-  );
-};
+//   return (
+//     <OverlayTrigger
+//       trigger={['hover', 'focus']}
+//       placement="left"
+//       overlay={statsPopover}
+//       onEnter={onEnter}
+//     >
+//       <span>
+//         <UserName user={user} />
+//       </span>
+//     </OverlayTrigger>
+//   );
+// }
 
+const UserInfo = ({ user }) => (
+  <span>
+    <UserName user={user} />
+  </span>
+);
 
 export default UserInfo;
