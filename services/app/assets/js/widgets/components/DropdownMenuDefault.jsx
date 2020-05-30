59790f312e9c2dcf1b6ebbb1f59b57e7dcfaa0a8
ReDBrother <bataevdmitriy@gmail.com>
2020-05-30 21:03:58 +0300

    refactor DropdownMenu

--- services/app/assets/js/widgets/components/DropdownMenuDefault.jsx
+++ services/app/assets/js/widgets/components/DropdownMenuDefault.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
+import i18n from '../../i18n';
+import levelToClass from '../config/levelToClass';
 
-const DropdownMenuDefault = ({ renderLevel }) => {
-  const orderedLevels = ['elementary', 'easy', 'medium', 'hard'];
-  return (
-    <>
-      <div className="dropdown-header">Select a difficulty</div>
-      <div className="dropdown-divider" />
-      {orderedLevels.map(renderLevel)}
-    </>
-  );
+
+const DropdownItem = ({ level, setLevel, setLevelClass }) => (
+  <button
+    key={level}
+    className="dropdown-item"
+    type="button"
+    onClick={() => {
+      setLevel(level);
+      setLevelClass(levelToClass[level]);
+    }}
+  >
+    <span className={`badge badge-pill badge-${levelToClass[level]} mr-1`}>&nbsp;</span>
+    {i18n.t(level)}
+  </button>
+);
+
+const DropdownMenuDefault = ({ currentLevel, setLevel, setLevelClass }) => {
+  const orderedLevels = ['random', 'elementary', 'easy', 'medium', 'hard'].filter(level => level !== currentLevel);
+
+  return orderedLevels.map(level => (
+    <DropdownItem
+      key={level}
+      level={level}
+      setLevel={setLevel}
+      setLevelClass={setLevelClass}
+    />
+  ));
 };
 
 export default DropdownMenuDefault;
