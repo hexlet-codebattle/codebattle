86088c4293b51ffb8884506d6ed9e207413ff56f
feycot <feycot@gmail.com>
2020-06-18 14:18:51 +0500

    add back to home translate

--- services/app/assets/js/widgets/components/Toast/BackToHomeButton.jsx
+++ services/app/assets/js/widgets/components/Toast/BackToHomeButton.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import i18n from '../../../i18n';
 
 export default function BackToHomeButton() {
+  const title = i18n.t('Back to Home');
   const handleClick = () => {
     window.location.href = '/';
   };
 
   return (
     <button className="btn btn-secondary btn-block" onClick={handleClick} type="button">
-      Back to home
+      {title}
     </button>
   );
 }
