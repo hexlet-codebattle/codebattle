3c4646fa00125fe3d8a444abe4e4b8b1dd5ef188
YuriySho <yuriy.shoshin.87@mail.ru>
2020-07-23 08:16:37 +0300

    added contributors list for tasks (#764)
    * added contributors list for tasks
    
    * fix jest test
    
    * fix codebeat
    
    * add test for ContributorsList
    
    * fix lint

---
+++ services/app/assets/js/__tests__/ContributorsList.test.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import axios from 'axios';
+
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+
+import reducers from '../widgets/slices';
+import ContributorsList from '../widgets/components/ContributorsList';
+
+jest.mock('gon', () => {
+  const gonParams = { local: 'en' };
+  return { getAsset: type => gonParams[type] };
+}, { virtual: true });
+
+jest.mock('axios');
+const users = [];
+axios.get.mockResolvedValue({ data: users });
+
+test('test rendering ContributorsList', async () => {
+  const reducer = combineReducers(reducers);
+
+  const preloadedState = {
+    name: '',
+  };
+  const store = configureStore({
+    reducer,
+    preloadedState,
+  });
+
+  const { getByText } = render(<Provider store={store}><ContributorsList /></Provider>);
+
+  expect(getByText(/This users have contributed to this task:/)).toBeInTheDocument();
+});
