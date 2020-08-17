9e1d86136544bbd4b1dac49081a0b436d72dc3ff
valerr <spokich394@gmail.com>
2020-08-17 15:07:41 +0300

    add mentions highlighting

--- services/app/assets/js/widgets/components/Message.jsx
+++ services/app/assets/js/widgets/components/Message.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
-const Message = ({ message = '', user = '' }) => (
-  <div>
-    <span className="font-weight-bold">{`${user}: `}</span>
-    <span>{message}</span>
-  </div>
-);
+const Message = ({ message = '', user = '' }) => {
+  const parts = message.split(/(@+[-a-zA-Z0-9_]+\b)/g);
+
+  return (
+    <div>
+      <span className="font-weight-bold">{`${user}: `}</span>
+      <span>
+        {/* eslint-disable-next-line react/no-array-index-key */}
+        {parts.map((part, i) => (part.slice(1) === user ? <span key={i} className="text-info">{part}</span> : `${part}`))}
+      </span>
+    </div>
+  );
+};
 
 export default Message;
