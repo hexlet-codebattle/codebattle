71ee8d4e4dfd1cfecd90acddf190bb11780c2e63
ReDBrother <bataevdmitriy@gmail.com>
2020-09-14 04:51:08 +0300

    udpate template of team tournament [add navs]

--- services/app/lib/codebattle_web/views/tournament_view.ex
+++ services/app/lib/codebattle_web/views/tournament_view.ex
@@ -7,6 +7,13 @@ defmodule CodebattleWeb.TournamentView do
 
   def render_datetime(nil), do: "none"
 
+  def render_average_time(ms) do
+    ms
+    |> Timex.Duration.from_milliseconds()
+    |> Timex.Duration.to_time!()
+    |> Timex.format!("%H:%M:%S:%L", :strftime)
+  end
+
   def render_datetime(utc_datetime) do
     utc_datetime
     |> Timex.Timezone.convert(@default_timezone)
