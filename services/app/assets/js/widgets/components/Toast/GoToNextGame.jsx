5beeac04fd52162896e6ddfc88369b7a09318ce0
enmalafeev <malafeev.en@yandex.ru>
2020-11-14 11:38:20 +0300

    refactor nextMatch in GoToNextGame button

--- services/app/assets/js/widgets/components/Toast/GoToNextGame.jsx
+++ services/app/assets/js/widgets/components/Toast/GoToNextGame.jsx
@@ -4,7 +4,7 @@ import _ from 'lodash';
 const GoToNextGame = ({ currentUserId, info: { state, data: { matches } } }) => {
   if (state !== 'active') return;
   const activeMatches = matches.filter(match => match.state === 'active');
-  const nextMatch = _.find(activeMatches, ({ players: [{ id: firstPlayerId }, { id: secondPlayerId }] }) => firstPlayerId === currentUserId || secondPlayerId === currentUserId);
+  const nextMatch = _.find(activeMatches, ({ players }) => players.some(({ id }) => id === currentUserId));
   return (
     nextMatch
     && (
