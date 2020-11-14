1bb7921dd68c92ee4e0396bb06b60829263dedd9
enmalafeev <malafeev.en@yandex.ru>
2020-11-14 13:59:29 +0300

    fix eslint errors in GoToNextGame.js

--- services/app/assets/js/widgets/components/Toast/GoToNextGame.jsx
+++ services/app/assets/js/widgets/components/Toast/GoToNextGame.jsx
@@ -2,16 +2,21 @@ import React from 'react';
 import _ from 'lodash';
 
 const GoToNextGame = ({ currentUserId, info: { state, data: { matches } } }) => {
-  if (state !== 'active') return;
+  if (state !== 'active') {
+    return null;
+  }
   const activeMatches = matches.filter(match => match.state === 'active');
   const nextMatch = _.find(activeMatches, ({ players }) => players.some(({ id }) => id === currentUserId));
   return (
-    nextMatch
-    && (
-    <a className="btn btn-success btn-block" href={`/games/${nextMatch.game_id}`}>
-      Go to next game
-    </a>
-    )
+    <>
+      {
+        nextMatch && (
+        <a className="btn btn-success btn-block" href={`/games/${nextMatch.game_id}`}>
+          Go to next game
+        </a>
+        )
+      }
+    </>
   );
 };
 
