519c023302b73a9af198cc5524bcaf4b2407c3b3
MityaDementiy <d.dementiy@gmail.com>
2020-12-21 21:51:51 +0200

    Add redirect to recently created game

--- services/app/assets/js/widgets/middlewares/Lobby.js
+++ services/app/assets/js/widgets/middlewares/Lobby.js
@@ -7,14 +7,37 @@ const channelName = 'lobby';
 const isRecord = Gon.getAsset('is_record');
 const channel = !isRecord ? socket.channel(channelName) : null;
 
-export const fetchState = () => dispatch => {
+export const fetchState = () => (dispatch, getState) => {
   const camelizeKeysAndDispatch = actionCreator => data => (
     dispatch(actionCreator(camelizeKeys(data)))
   );
 
   channel.join().receive('ok', camelizeKeysAndDispatch(actions.initGameList));
 
-  channel.on('game:upsert', camelizeKeysAndDispatch(actions.upsertGameLobby));
+  channel.on('game:upsert', data => {
+    const { game: { players, id, state: gameStatus } } = data;
+    const isRedirecting = () => {
+      if (!gameStatus === 'playing') {
+        return false;
+      }
+      if (players.length < 2) {
+        return false;
+      }
+      const currentGamePlayersIds = players.map(player => player.id);
+      const currentPlayerId = getState().user.currentUserId;
+      if (!currentGamePlayersIds.includes(currentPlayerId)) {
+        return false;
+      }
+      return true;
+    };
+    const redirectToGame = isRedirecting();
+    if (redirectToGame) {
+      window.location.href = `/games/${id}`;
+    } else {
+      dispatch(actions.upsertGameLobby(camelizeKeys(data)));
+    }
+  });
+
   channel.on('game:remove', camelizeKeysAndDispatch(actions.removeGameLobby));
   channel.on('game:finish', camelizeKeysAndDispatch(actions.finishGame));
 };
