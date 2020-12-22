c8e922f3b2a493744ab0b7b060822f2e09c6eb36
Andrey Karamnoff <karamnoff@gmail.com>
2020-12-22 13:33:37 +0300

    fix UI for TopPlayersPerPeriod

--- services/app/assets/js/widgets/components/TopPlayers/TopPlayersPerPeriod.jsx
+++ services/app/assets/js/widgets/components/TopPlayers/TopPlayersPerPeriod.jsx
@@ -3,6 +3,7 @@ import { Table } from 'react-bootstrap';
 import qs from 'qs';
 import moment from 'moment';
 import axios from 'axios';
+import classnames from 'classnames';
 import UserInfo from '../../containers/UserInfo';
 
 const periodType = {
@@ -21,6 +22,23 @@ const TopPlayersPerPeriod = () => {
   const [period, setPeriod] = useState(periodType.MONTHLY);
 
   const periodRef = useRef(null);
+
+  const handlePeriodClick = ({ target: { textContent } }) => {
+    const periodValue = textContent && textContent.trim();
+
+    switch (periodValue) {
+      case periodType.MONTHLY:
+        setPeriod(periodType.MONTHLY);
+        break;
+
+      case periodType.WEEKLY:
+        setPeriod(periodType.WEEKLY);
+        break;
+
+      default:
+        throw new Error(`Unknown period: ${periodValue}`);
+    }
+  };
 
   useEffect(() => {
     const queryParamsString = qs.stringify({
@@ -46,41 +64,45 @@ const TopPlayersPerPeriod = () => {
       <thead>
         <tr className="bg-gray">
           <th scope="col" className="text-uppercase p-1" colSpan="2">
-            <img
-              alt="rating"
-              src="/assets/images/topPlayers.svg"
-              className="m-2"
-            />
-            Leaderboard
-            {' '}
-            <u>
-              <a
-                href="#!"
-                ref={periodRef}
-                onClick={e => {
-                  e.preventDefault();
-
-                  const {
-                    target: { textContent: periodValue },
-                  } = e;
-
-                  switch (periodValue.trim()) {
-                    case periodType.MONTHLY:
-                      setPeriod(periodType.WEEKLY);
-                      break;
-
-                    case periodType.WEEKLY:
-                      setPeriod(periodType.MONTHLY);
-                      break;
-
-                    default:
-                      throw new Error(`Unknown period: ${periodValue.trim()}`);
-                  }
-                }}
-              >
-                {period}
-              </a>
-            </u>
+            <div className="d-flex align-items-center flex-nowrap">
+              <img
+                alt="rating"
+                src="/assets/images/topPlayers.svg"
+                className="m-2"
+              />
+              <p className="d-inline-flex align-items-baseline flex-nowrap m-0 p-0">
+                <span className="d-flex">Leaderboard&thinsp;</span>
+                <span className="small d-flex">
+                  <u>
+                    <a
+                      href="#!"
+                      ref={periodRef}
+                      onClick={handlePeriodClick}
+                      className={classnames({
+                        'text-orange': period === periodType.MONTHLY,
+                        'text-black': period !== periodType.MONTHLY,
+                      })}
+                    >
+                      {periodType.MONTHLY}
+                    </a>
+                  </u>
+                  /
+                  <u>
+                    <a
+                      href="#!"
+                      ref={periodRef}
+                      onClick={handlePeriodClick}
+                      className={classnames({
+                        'text-orange': period === periodType.WEEKLY,
+                        'text-black': period !== periodType.WEEKLY,
+                      })}
+                    >
+                      {periodType.WEEKLY}
+                    </a>
+                  </u>
+                </span>
+              </p>
+            </div>
           </th>
         </tr>
       </thead>
