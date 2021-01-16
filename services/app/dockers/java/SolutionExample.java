59b97f9566b9a63f6f2e2fd327fef37666bf4dc2
vtm9 <vtmilyakov@yandex.ru>
2021-01-16 09:47:34 +0100

    Improve java checker

--- services/app/dockers/java/SolutionExample.java
+++ services/app/dockers/java/SolutionExample.java
@@ -1,6 +1,6 @@
 package solution;
 
-public class Solution {
+public class SolutionExample {
     public int solution(int a, int b) {
         System.out.println("Hello World");
         return a + b;
