2e8f0083c3043ca56ee63213c6bd83788372856b
solar05 <artem00298@gmail.com>
2021-01-21 00:25:50 +0300

    ADd default value for migration

--- services/app/priv/repo/migrations/20210119140554_update_tasks_table.exs
+++ services/app/priv/repo/migrations/20210119140554_update_tasks_table.exs
@@ -3,8 +3,8 @@ defmodule Codebattle.Repo.Migrations.UpdateTasksTable do
 
   def change do
     alter table(:tasks) do
-      add :description_ru, :text
-      add :description_en, :text
+      add :description_ru, :text, default: "Описание отсутствует."
+      add :description_en, :text, default: "No description available."
     end
 
     rename table(:tasks), :description, to: :examples
