14f2802463a959540dbdd3215476c9a8d1c5debd
Dmitriy Bataev <bataevdmitriy@gmail.com>
2021-04-21 19:36:49 +0300

    remove unused useContext [RootContainer]

--- services/app/assets/js/widgets/containers/RootContainer.jsx
+++ services/app/assets/js/widgets/containers/RootContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect, useSelector, useDispatch } from 'react-redux';
 import Gon from 'gon';
