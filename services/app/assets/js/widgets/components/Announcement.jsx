da4f225f15dad4666f013754b791e324da6671c0
natalialukashova <naluva.work@gmail.com>
2021-06-07 18:40:11 +0300

    stylized tr

--- services/app/assets/js/widgets/components/Announcement.jsx
+++ services/app/assets/js/widgets/components/Announcement.jsx
@@ -14,11 +14,11 @@ const Announcement = () => (
     </thead>
     <tbody>
       <tr>
-        <td className="pr-0"><p className="m-0">Monthly arena 2021.06.14 16:00(UTC)</p></td>
+        <td className="pr-0">Monthly arena 2021.06.14 16:00(UTC)</td>
         <td className="text-right pl-0" />
       </tr>
       <tr>
-        <td className="pr-0"><p className="m-0">Monthly Team Skirmish 2021.06.14 16:30(UTC)</p></td>
+        <td className="pr-0">Monthly Team Skirmish 2021.06.14 16:30(UTC)</td>
         <td className="text-right pl-0" />
       </tr>
     </tbody>
