d130e58ead1635db99d8b09be8e1996f96022b78
valerr <spokich394@gmaill.com>
2021-06-25 11:41:36 +0300

    add UserInfo component, change JoinButton

--- services/app/assets/js/widgets/containers/Participants.jsx
+++ services/app/assets/js/widgets/containers/Participants.jsx
@@ -1,57 +1,42 @@
 import React from 'react';
 import Gon from 'gon';
+import UserInfo from './UserInfo';
 
 const Participants = tournament => {
   const { tournament: { data: { players }, creatorId } } = tournament;
   const currentUser = Gon.getAsset('current_user');
   const { id } = currentUser;
-  const isCurrentUserParticipant = players.find(item => item.id === id);
+  const isParticipant = players.some(item => item.id === id);
+  const isShow = tournament.tournament.state === 'waiting_participants';
 
-  const renderJoinButton = () => {
-     if (isCurrentUserParticipant) {
-            return (
-              <button type="button" className="btn btn-outline-danger">Leave</button>
-            );
-          }
-            return (
-              <button type="button" className="btn btn-outline-secondary">
-                Join
-              </button>
-            );
+  // eslint-disable-next-line no-shadow
+  const JoinButton = ({ isShow, isParticipant /* onJoin, onLeave */ }) => {
+      if (!isShow) {
+        return null;
+      }
+      // const onClick = isParticipant ? onLeave : onJoin;
+      const text = isParticipant ? 'Leave' : 'Join';
+      return (
+        <button type="button" className={`btn ${isParticipant ? 'btn-outline-danger' : 'btn-outline-secondary'}`}>{text}</button>
+      );
   };
-
-  const renderPlayer = player => (
-    <>
-      <div className="d-flex align-items-center">
-        <a className="d-inline-flex.align-items-center" href={`/users/${player.id}`}>
-          <img
-            className="attachment rounded border mr-1 cb-user-avatar"
-            src={`https://avatars0.githubusercontent.com/u/${player.githubId}`}
-            alt={player.name}
-          />
-          <span className="mr-1 text-truncate" style={{ maxWidth: '130px' }}>{player.name}</span>
-        </a>
-        <small className="mr-1">{player.rating}</small>
-      </div>
-      {creatorId === id && tournament.tournament.state === 'waiting_participants' && (
-        <button type="button" className="btn btn-outline-danger">
-          Kick
-        </button>
-      )}
-    </>
-  );
 
   return (
     <div className="container mt-2 bg-white shadow-sm p-2">
       <div className="d-flex align-items-center flex-wrap justify-content-start">
         <h5 className="mb-2 mr-5">Participants</h5>
-        {tournament.tournament.state === 'waiting_participants' && renderJoinButton()}
+        {JoinButton({ isShow, isParticipant /* onJoin, onLeave, */ })}
       </div>
       <div className="my-3">
         {players.map(player => (
           <div className="my-3 d-flex" key={player.id}>
             <div className="d-flex align-items-center">
-              {renderPlayer(player)}
+              <UserInfo user={player} />
+              {creatorId === id && isShow && (
+                <button type="button" className="btn btn-outline-danger ml-2">
+                  Kick
+                </button>
+              )}
             </div>
           </div>
         ))}
