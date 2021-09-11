83e18ff59765d0c9a4e304e1052875f34b7cd576
vtm9 <vtmilyakov@yandex.ru>
2021-09-11 21:13:50 +0300

    Add pagination from scratch
    Thx to Serega Krivtsov!

--- services/app/lib/codebattle_web/controllers/api/v1/user_controller.ex
+++ services/app/lib/codebattle_web/controllers/api/v1/user_controller.ex
@@ -7,11 +7,15 @@ defmodule CodebattleWeb.Api.V1.UserController do
   import Ecto.Query, warn: false
 
   def index(conn, params) do
-    page_number = Map.get(params, "page", "1")
+    page_number =
+      params
+      |> Map.get("page", "1")
+      |> String.to_integer
 
     page_size =
       params
       |> Map.get("page_size", "50")
+      |> String.to_integer
 
     query = Codebattle.User.Scope.list_users(params)
     page = Repo.paginate(query, %{page: page_number, page_size: page_size})
