763c7b23d4893a95800c28db2643712eb1ef001c
PromiSe#### <vicimpa@ya.ru>
2022-04-11 10:24:14 +0300

    Lint fix

--- services/app/assets/js/widgets/slices/completedGames.js
+++ services/app/assets/js/widgets/slices/completedGames.js
@@ -1,6 +1,6 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import axios from "axios";
-import { camelizeKeys } from "humps";
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { camelizeKeys } from 'humps';
 
 export const fetchCompletedGames = createAsyncThunk(
   'completedGames/fetchCompletedGames',
