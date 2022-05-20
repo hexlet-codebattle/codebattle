4aa51870e8995d22d836b22379153cfaccea1c80
Andrey Grozs <grozwalker@gmail.com>
2022-05-20 11:24:18 +0000

    update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,104 @@
-variable "codebattle_port" {}
-variable "codebattle_secret_key_base" {}
-variable "codebattle_live_view_salt" {}
-variable "codebattle_db_hostname" {}
-variable "codebattle_db_username" {}
-variable "codebattle_db_password" {}
-variable "codebattle_db_name" {}
-variable "codebattle_db_port" {}
-variable "github_client_secret" {}
-variable "github_client_id" {}
-variable "discord_client_secret" {}
-variable "discord_client_id" {}
-variable "onesignal_api_key" {}
-variable "onesignal_app_id" {}
-variable "firebase_api_key" {}
-variable "firebase_sender_id" {}
-variable "rollbar_api_key" {}
+# Define K8s Region
+variable "cluster_region" {
+  type = string
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "cluster_node_name" {
+  type = string
+}
+
+variable "cluster_node_size" {
+  type = string
+}
+
+variable "postgres_db_node_size" {
+  type = string
+}
+
+variable "postgres_version" {
+  type = string
+}
+
+variable "postgres_db_cluster_name" {
+  type = string
+}
+
+variable "rel_path_to_kubeconfig" {
+  type        = string
+  default     = "../.kube/config"
+  description = "Path to kubeconfig file"
+}
+
+variable "write_kubeconfig" {
+  type        = bool
+  default     = true
+}
+
+variable "codebattle_port" {
+  type = string
+}
+
+variable "codebattle_secret_key_base" {
+  type = string
+}
+
+variable "codebattle_live_view_salt" {
+  type = string
+}
+
+variable "codebattle_db_username" {
+  type = string
+}
+
+variable "codebattle_db_password" {
+  type = string
+}
+
+variable "codebattle_db_name" {
+  type = string
+}
+
+variable "codebattle_db_port" {
+  type = string
+}
+
+variable "github_client_secret" {
+  type = string
+}
+
+variable "github_client_id" {
+  type = string
+}
+
+variable "discord_client_secret" {
+  type = string
+}
+
+variable "discord_client_id" {
+  type = string
+}
+
+variable "onesignal_api_key" {
+  type = string
+}
+
+variable "onesignal_app_id" {
+  type = string
+}
+
+variable "firebase_api_key" {
+  type = string
+}
+
+variable "firebase_sender_id" {
+  type = string
+}
+
+variable "rollbar_api_key" {
+  type = string
+  description = "Postgres DB Cluster name"
+}
