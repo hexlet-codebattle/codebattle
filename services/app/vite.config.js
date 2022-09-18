56c9e7caa0c8b90f34a4b75543e1dc2e4119b9fc
Victor Kasap <kasap.victor@gmail.com>
2022-09-18 15:06:26 +0300

    wip #1152: Added aliases for build

--- services/app/vite.config.js
+++ services/app/vite.config.js
@@ -5,13 +5,15 @@ export default {
   resolve: {
     alias: {
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+      '~nprogress': path.resolve(__dirname, 'node_modules/nprogress'),
+      '~font-mfizz': path.resolve(__dirname, 'node_modules/font-mfizz'),
     },
   },
   publicDir: './static',
   build: {
     target: 'es2018',
     minify: true,
-    outDir: 'priv2/static',
+    outDir: '../priv2/static',
     emptyOutDir: true,
     rollupOptions: {
       input: ['./assets/js/app.js', './assets/css/style.scss'],
