e378159e09ef089509269dd5d9469116b07b856a
vtm9 <vtmilyakov@gmail.com>
2022-10-13 10:09:15 +0300

    Improve rank update server

--- services/app/lib/codebattle/users_rank_update_server.ex
+++ services/app/lib/codebattle/users_rank_update_server.ex
@@ -5,6 +5,8 @@ defmodule Codebattle.UsersRankUpdateServer do
 
   require Logger
 
+  @timeout :timer.minutes(15)
+
   # API
   def start_link(_) do
     GenServer.start_link(__MODULE__, [], name: __MODULE__)
@@ -16,6 +18,8 @@ defmodule Codebattle.UsersRankUpdateServer do
 
   # SERVER
   def init(_) do
+    Process.send_after(self(), :work, @timeout)
+
     Codebattle.PubSub.subscribe("main")
     Codebattle.PubSub.subscribe("games")
     Logger.info("Start UsersRankServer")
@@ -23,14 +27,25 @@ defmodule Codebattle.UsersRankUpdateServer do
   end
 
   def handle_cast(:update, state) do
-    Codebattle.User.RankUpdate.call()
-    Logger.info("Rank has been recalculated")
+    do_work()
+    {:noreply, state}
+  end
 
+  def handle_info(:work, state) do
+    do_work()
+    Process.send_after(self(), :work, @timeout)
     {:noreply, state}
   end
 
   def handle_info(%{event: "game:finished"}, state) do
-    Codebattle.User.RankUpdate.call()
+    do_work()
     {:noreply, state}
+  end
+
+  def handle_info(_, state), do: {:noreply, state}
+
+  defp do_work do
+    Codebattle.User.RankUpdate.call()
+    Logger.info("Rank has been recalculated")
   end
 end
