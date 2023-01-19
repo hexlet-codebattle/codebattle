c9abd5a1945cc4008fd5eba3b2d62cad4c4ddc07
Guryanov-Maksim <guryanov-mv@yandex.ru>
2023-01-19 13:24:27 +0300

    Replace tabs with spaces in the editor when it is required

---
+++ services/app/assets/js/widgets/config/langToSpacesMapping.js
@@ -0,0 +1,14 @@
+export default {
+  js: true,
+  ts: true,
+  cpp: true,
+  csharp: true,
+  java: true,
+  kotlin: true,
+  haskell: true,
+  ruby: true,
+  elixir: true,
+  php: true,
+  python: true,
+  clojure: true,
+};
--- services/app/assets/js/widgets/utils/editor.js
+++ services/app/assets/js/widgets/utils/editor.js
@@ -1,9 +1,12 @@
 import languageTabSizes from '../config/languageTabSizes';
+import langToSpacesMapping from '../config/langToSpacesMapping';
 
 const getLanguageTabSize = language => {
   const defaultTabSize = 2;
 
   return languageTabSizes[language] || defaultTabSize;
 };
+
+export const shouldReplaceTabsWithSpaces = language => langToSpacesMapping[language] || false;
 
 export default getLanguageTabSize;
