d9a6ab16bd4b01e7afabeb1754be193cb75340ba
Guryanov-Maksim <guryanov-mv@yandex.ru>
2023-02-17 10:43:43 +0300

    Fix missed language icons

---
+++ services/app/apps/codebattle/assets/js/widgets/config/langIconNames.js
@@ -0,0 +1,17 @@
+export default {
+  js: 'js',
+  javascript: 'js',
+  ts: 'ts',
+  cpp: 'cpp',
+  csharp: 'csharp',
+  dart: 'dart',
+  golang: 'golang',
+  java: 'java',
+  kotlin: 'kotlin',
+  haskell: 'haskell',
+  ruby: 'ruby',
+  elixir: 'elixir',
+  php: 'php',
+  python: 'python',
+  clojure: 'clojure',
+};
