efedb1079117d181ff0a94a12ac3feed82d59955
Guryanov-Maksim <guryanov-mv@yandex.ru>
2023-02-26 18:18:41 +0300

    Add tests for wrong language selector values

--- services/app/apps/codebattle/assets/js/__tests__/UserSettings.test.jsx
+++ services/app/apps/codebattle/assets/js/__tests__/UserSettings.test.jsx
@@ -8,6 +8,7 @@ import { Provider } from 'react-redux';
 import reducers from '../widgets/slices';
 import UserSettings from '../widgets/containers/UserSettings';
 import UserSettingsForm from '../widgets/components/User/UserSettingsForm';
+import languages from '../widgets/config/languages';
 
 jest.mock('@fortawesome/react-fontawesome', () => ({
   FontAwesomeIcon: 'img',
@@ -49,6 +50,8 @@ jest.mock(
   { virtual: true },
 );
 
+const settings = Object.keys(languages);
+
 const vdom = () => render(
   <Provider store={store}>
     <UserSettings />
@@ -69,7 +72,7 @@ describe('UserSettings test cases', () => {
 
     setTimeout(() => expect(alert).toHaveClass('editSuccess'), 300);
   });
-  it('editing profile test', async () => {
+  test.each(settings)('editing profile test with lang %s', async lang => {
     const handleSubmit = jest.fn();
     render(
       <UserSettingsForm
@@ -81,7 +84,7 @@ describe('UserSettings test cases', () => {
       target: { value: 'Dmitry' },
     });
     fireEvent.change(screen.getByTestId('langSelect'), {
-      target: { value: 'java' },
+      target: { value: lang },
     });
     const saveBtn = screen.getByRole('button', { name: /save/i });
     fireEvent.click(saveBtn);
@@ -89,7 +92,7 @@ describe('UserSettings test cases', () => {
       expect(handleSubmit).toHaveBeenCalled();
       expect(handleSubmit).toHaveBeenCalledWith({
         name: 'Dmitry',
-        lang: 'java',
+        lang,
         sound_settings: {
           level: 6,
           type: 'standart',
