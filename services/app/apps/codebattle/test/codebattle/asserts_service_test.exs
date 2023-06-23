0646cf729e7929553570cdd991feec1ba492a3ff
solar05 <artem00298@gmail.com>
2023-06-23 18:04:29 +0300

    Add more test

--- services/app/apps/codebattle/test/codebattle/asserts_service_test.exs
+++ services/app/apps/codebattle/test/codebattle/asserts_service_test.exs
@@ -25,6 +25,13 @@ defmodule Codebattle.AssertsServiceTest do
       assert AssertsService.valid_asserts?(asserts, input_arguments)
     end
 
+    test "floats" do
+      input_arguments = %{"argument-name" => "some", "name" => "float"}
+      asserts = [1.2]
+
+      assert AssertsService.valid_asserts?(asserts, input_arguments)
+    end
+
     test "arrays" do
       input_arguments = %{
         "argument-name" => "some",
@@ -33,6 +40,18 @@ defmodule Codebattle.AssertsServiceTest do
       }
 
       asserts = [[1, 2, 3]]
+
+      assert AssertsService.valid_asserts?(asserts, input_arguments)
+    end
+
+    test "hashes" do
+      input_arguments = %{
+        "argument-name" => "some",
+        "name" => "hash",
+        "nested" => %{"name" => "integer"}
+      }
+
+      asserts = [%{"some" => 1}]
 
       assert AssertsService.valid_asserts?(asserts, input_arguments)
     end
@@ -47,6 +66,59 @@ defmodule Codebattle.AssertsServiceTest do
       asserts = [[[1, 2, 3]]]
 
       assert AssertsService.valid_asserts?(asserts, input_arguments)
+    end
+  end
+
+  describe ".type_asserts" do
+    test "integers" do
+      asserts = [1]
+
+      assert AssertsService.type_asserts(asserts) = %{"name" => "integer"}
+    end
+
+    test "booleans" do
+      asserts = [true]
+
+      assert AssertsService.type_asserts(asserts) = %{"name" => "boolean"}
+    end
+
+    test "strings" do
+      asserts = ["some"]
+
+      assert AssertsService.type_asserts(asserts) = %{"name" => "string"}
+    end
+
+    test "floats" do
+      asserts = [1.1]
+
+      assert AssertsService.type_asserts(asserts) = %{"name" => "float"}
+    end
+
+    test "arrays" do
+      asserts = [[1, 2]]
+
+      assert AssertsService.type_asserts(asserts) = %{
+               "name" => "array",
+               "nested" => %{"name" => "integer"}
+             }
+    end
+
+    test "hashes" do
+      asserts = [%{"some" => 1}]
+
+      assert AssertsService.type_asserts(asserts) = %{
+               "name" => "hash",
+               "nested" => %{"name" => "integer"}
+             }
+    end
+
+    test "nested arrays" do
+      asserts = [[[1, 2, 3]]]
+
+      assert AssertsService.type_asserts(asserts) = %{
+               "name" => "array",
+               "nested" => %{"name" => "array", "nested" => %{"name" => "integer"}}
+             }
     end
   end
 end
