3bf9b61062b039772e3496587bc6047cc7fd8d55
Eugene Sinitsyn <sinitsyn@sportxline.com>
2023-07-01 16:20:43 +0500

    Add routes and test drafts

--- services/app/apps/codebattle/priv/repo/migrations/20230630133912_create_player_report.exs
+++ services/app/apps/codebattle/priv/repo/migrations/20230630133912_create_player_report.exs
@@ -2,7 +2,7 @@ defmodule Codebattle.Repo.Migrations.CreatePlayerReport do
   use Ecto.Migration
 
   def change do
-    create table(:player_report) do
+    create table(:player_reports) do
       add :user_id, :integer
       add :game_id, :integer
       add :reason, :string
