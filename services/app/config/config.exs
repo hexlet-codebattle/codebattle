460b9fbd8aa17438b4b55302f277a99c01cd0e48
vtm9 <vtmilyakov@gmail.com>
2023-07-23 18:58:59 +0300

    Update elixir version

--- services/app/config/config.exs
+++ services/app/config/config.exs
@@ -29,8 +29,10 @@ config :logger, :console,
 
 config :phoenix, :json_library, Jason
 
+config :codebattle, github_oauth_client: HTTPoison
+config :codebattle, discord_oauth_client: HTTPoison
+
 config :codebattle, :oauth,
-  mock_clinet: false,
   github_client_id: System.get_env("GITHUB_CLIENT_ID", "ASFD"),
   github_client_secret: System.get_env("GITHUB_CLIENT_SECRET", "ASFD"),
   discord_client_id: System.get_env("DISCORD_CLIENT_ID", "ASFD"),
