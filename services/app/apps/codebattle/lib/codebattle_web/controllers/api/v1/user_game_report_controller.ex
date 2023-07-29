31647546d36ef6b68ad8a9594f94d8066c6ad720
Eugene Sinitsyn <djorka91@mail.ru>
2023-07-29 13:46:14 +0500

    Add more tests

--- services/app/apps/codebattle/lib/codebattle_web/controllers/api/v1/user_game_report_controller.ex
+++ services/app/apps/codebattle/lib/codebattle_web/controllers/api/v1/user_game_report_controller.ex
@@ -1,36 +1,37 @@
 defmodule CodebattleWeb.Api.V1.UserGameReportController do
   use CodebattleWeb, :controller
 
-  # import Ecto.Changeset
+  import Ecto.Changeset
 
   alias Codebattle.Game
   alias Codebattle.UserGameReport
 
-  def create(
-        conn,
-        %{
-          "id" => game_id,
-          "user_id" => reported_user_id,
-          "reason" => reason,
-          "comment" => comment
-        }
-      ) do
+  def create(conn, %{
+        "id" => game_id,
+        "user_id" => reported_user_id,
+        "reason" => reason,
+        "comment" => comment
+      }) do
     reporter = conn.assigns.current_user
     game = Game.Context.get_game!(game_id)
 
     is_reporter_player = Game.Helpers.is_player?(game, reporter.id)
     is_reported_user_player = Game.Helpers.is_player?(game, reported_user_id)
+    is_reported_himself = reporter.id == reported_user_id
 
-    case {is_reporter_player, is_reported_user_player} do
-      {false, _} ->
-        conn |> put_status(:forbidden) |> json(%{errors: [:not_a_player_of_game]})
+    case {is_reporter_player, is_reported_himself, is_reported_user_player} do
+      {false, _, _} ->
+        conn |> put_status(:forbidden) |> json(%{errors: ["not_a_player_of_game"]})
 
-      {true, false} ->
+      {_, true, _} ->
+        conn |> put_status(:unprocessable_entity) |> json(%{errors: ["cannot_report_himself"]})
+
+      {_, _, false} ->
         conn
         |> put_status(:unprocessable_entity)
         |> json(%{errors: [:reported_user_not_a_player_of_game]})
 
-      {true, true} ->
+      {_, _, true} ->
         case UserGameReport.create(%{
                game_id: game.id,
                reporter_id: reporter.id,
--- services/app/apps/codebattle/test/codebattle_web/controllers/api/v1/user_game_report_controller_test.exs
+++ services/app/apps/codebattle/test/codebattle_web/controllers/api/v1/user_game_report_controller_test.exs
@@ -51,10 +51,130 @@ defmodule CodebattleWeb.Api.V1.UserGameReportControllerTest do
       assert user_game_report.reported_user.id == bot.id
     end
 
-    test "player cannot report himself" do
+    test "player cannot report himself", %{conn: conn} do
+      user = insert(:user)
+      bot = Bot.Context.build()
+
+      task = insert(:task)
+
+      game_params = %{state: "playing", players: [user, bot], task: task}
+
+      {:ok, game} = Game.Context.create_game(game_params)
+
+      params = %{
+        "user_id" => user.id,
+        "reason" => "cheating",
+        "comment" => "Bot is cheating"
+      }
+
+      response =
+        conn
+        |> put_session(:user_id, user.id)
+        |> post(Routes.api_v1_user_game_report_path(conn, :create, game.id), params)
+        |> json_response(422)
+
+      assert %{"errors" => ["cannot_report_himself"]} = response
     end
 
-    test "player cannot report when game does not exists" do
+    test "player cannot report other player which is not game player", %{conn: conn} do
+      user = insert(:user)
+      other_user = insert(:user)
+      bot = Bot.Context.build()
+
+      task = insert(:task)
+
+      game_params = %{state: "playing", players: [user, bot], task: task}
+
+      {:ok, game} = Game.Context.create_game(game_params)
+
+      params = %{
+        "user_id" => other_user.id,
+        "reason" => "cheating",
+        "comment" => "Bot is cheating"
+      }
+
+      response =
+        conn
+        |> put_session(:user_id, user.id)
+        |> post(Routes.api_v1_user_game_report_path(conn, :create, game.id), params)
+        |> json_response(422)
+
+      assert %{"errors" => ["reported_user_not_a_player_of_game"]} = response
+    end
+
+    test "player which is not game player cannot report player of the game", %{conn: conn} do
+      other_user = insert(:user)
+      user = insert(:user)
+      bot = Bot.Context.build()
+
+      task = insert(:task)
+
+      game_params = %{state: "playing", players: [user, bot], task: task}
+
+      {:ok, game} = Game.Context.create_game(game_params)
+
+      params = %{
+        "user_id" => user.id,
+        "reason" => "cheating",
+        "comment" => "Bot is cheating"
+      }
+
+      response =
+        conn
+        |> put_session(:user_id, other_user.id)
+        |> post(Routes.api_v1_user_game_report_path(conn, :create, game.id), params)
+        |> json_response(403)
+
+      assert %{"errors" => ["not_a_player_of_game"]} = response
+    end
+
+    test "unprocessable entity with bad request", %{conn: conn} do
+      user = insert(:user)
+      bot = Bot.Context.build()
+
+      task = insert(:task)
+
+      game_params = %{state: "playing", players: [user, bot], task: task}
+
+      {:ok, game} = Game.Context.create_game(game_params)
+
+      params = %{
+        "user_id" => bot.id,
+        "reason" => "bot_cheated"
+      }
+
+      response =
+        conn
+        |> put_session(:user_id, user.id)
+        |> post(Routes.api_v1_user_game_report_path(conn, :create, game.id), params)
+        |> json_response(422)
+
+      assert %{"errors" => ["invalid_params"]} = response
+    end
+
+    test "unprocessable entity with incorrect reason", %{conn: conn} do
+      user = insert(:user)
+      bot = Bot.Context.build()
+
+      task = insert(:task)
+
+      game_params = %{state: "playing", players: [user, bot], task: task}
+
+      {:ok, game} = Game.Context.create_game(game_params)
+
+      params = %{
+        "user_id" => bot.id,
+        "reason" => "not_existed_reason",
+        "comment" => "Some comment"
+      }
+
+      response =
+        conn
+        |> put_session(:user_id, user.id)
+        |> post(Routes.api_v1_user_game_report_path(conn, :create, game.id), params)
+        |> json_response(422)
+
+      assert %{"errors" => %{"reason" => ["is invalid"]}} = response
     end
   end
 end
