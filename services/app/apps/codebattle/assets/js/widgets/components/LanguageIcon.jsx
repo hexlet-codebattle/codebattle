f039ddd8f35641dc8dd4fd7637d15c5eaa2c04fc
ushachev <vit.ush@mail.ru>
2023-12-27 12:47:06 +0300

    [#1685] replace library Font-mfizz with Devicons

--- services/app/apps/codebattle/assets/js/widgets/components/LanguageIcon.jsx
+++ services/app/apps/codebattle/assets/js/widgets/components/LanguageIcon.jsx
@@ -1,28 +1,51 @@
 import React from 'react';
 
-import cn from 'classnames';
+import ClojureOriginalIcon from 'react-devicons/clojure/original';
+import CppOriginalIcon from 'react-devicons/cplusplus/original';
+import CsharpOriginalIcon from 'react-devicons/csharp/original';
+import DartOriginalIcon from 'react-devicons/dart/original';
+import GolangOriginalIcon from 'react-devicons/go/original';
+import HaskellOriginalIcon from 'react-devicons/haskell/original';
+import JavaOriginalIcon from 'react-devicons/java/original';
+import NodejsPlainIcon from 'react-devicons/nodejs/plain';
+import TypescriptOriginalIcon from 'react-devicons/typescript/original';
 
-const iconsToClass = {
-  js: 'icon-nodejs',
-  javascript: 'icon-nodejs',
-  ts: 'icon-nodejs',
-  typescript: 'icon-nodejs',
-  dart: 'icon-ghost',
-  golang: 'icon-go',
-  cpp: 'icon-cplusplus',
-  csharp: 'icon-csharp',
-  java: 'icon-java',
-  kotlin: 'icon-java-bold',
-  ruby: 'icon-ruby',
-  elixir: 'icon-elixir',
-  haskell: 'icon-haskell',
-  clojure: 'icon-clojure',
-  python: 'icon-python',
-  php: 'icon-php-alt',
+import ElixirOriginalIcon from './icons/ElixirOriginalIcon';
+import KotlinOriginalIcon from './icons/KotlinOriginalIcon';
+import PhpOriginalIcon from './icons/PhpOriginalIcon';
+import PythonOriginalIcon from './icons/PythonOriginalIcon';
+import RubyOriginalIcon from './icons/RubyOriginalIcon';
+
+const iconRenderers = {
+  js: className => <NodejsPlainIcon className={className} color="green" size="1.25em" />,
+  javascript: className => <NodejsPlainIcon className={className} color="green" size="1.25em" />,
+  ts: className => <TypescriptOriginalIcon className={className} size="1.125em" />,
+  typescript: className => <TypescriptOriginalIcon className={className} size="1.125em" />,
+  dart: className => <DartOriginalIcon className={className} size="1.125em" />,
+  golang: className => <GolangOriginalIcon className={className} size="1.25em" />,
+  cpp: className => <CppOriginalIcon className={className} size="1.25em" />,
+  csharp: className => <CsharpOriginalIcon className={className} size="1.25em" />,
+  java: className => (
+    <JavaOriginalIcon
+      className={className}
+      style={{ transform: 'translateY(-0.125em)' }}
+      size="1.25em"
+    />
+  ),
+  kotlin: className => <KotlinOriginalIcon className={className} />,
+  ruby: className => <RubyOriginalIcon className={className} />,
+  elixir: className => <ElixirOriginalIcon className={className} size="1.25em" />,
+  haskell: className => <HaskellOriginalIcon className={className} size="1.125em" />,
+  clojure: className => <ClojureOriginalIcon className={className} size="1.125em" />,
+  python: className => <PythonOriginalIcon className={className} size="1.25em" />,
+  php: className => <PhpOriginalIcon className={className} size="1.875em" />,
+  default: () => null,
 };
 
-const LanguageIcon = ({ lang }) => (
-  <span className={cn('d-inline-block', iconsToClass[lang])} style={{ marginTop: 2 }} />
-);
+const LanguageIcon = ({ className = '', lang }) => {
+  const renderIcon = iconRenderers[lang] || iconRenderers.default;
+
+  return renderIcon(className);
+};
 
 export default LanguageIcon;
---
+++ services/app/apps/codebattle/assets/js/widgets/components/icons/ElixirOriginalIcon.jsx
@@ -0,0 +1,155 @@
+/* eslint-disable max-len */
+import React from 'react';
+
+const ElixirOriginalIcon = ({ className, size = '1em' }) => (
+  <svg viewBox="0 0 128 128" xmlns="http://www.w3.org/2000/svg" className={className} style={{ width: size, height: size }}>
+    <linearGradient
+      id="elixir-original-a"
+      gradientUnits="userSpaceOnUse"
+      x1="835.592"
+      y1="-36.546"
+      x2="821.211"
+      y2="553.414"
+      gradientTransform="matrix(.1297 0 0 .2 -46.03 17.198)"
+    >
+      <stop offset="0" stopColor="#d9d8dc" />
+      <stop offset="1" stopColor="#fff" stopOpacity=".385" />
+    </linearGradient>
+    <linearGradient
+      id="elixir-original-b"
+      gradientUnits="userSpaceOnUse"
+      x1="942.357"
+      y1="-40.593"
+      x2="824.692"
+      y2="472.243"
+      gradientTransform="matrix(.1142 0 0 .2271 -47.053 17.229)"
+    >
+      <stop offset="0" stopColor="#8d67af" stopOpacity=".672" />
+      <stop offset="1" stopColor="#9f8daf" />
+    </linearGradient>
+    <linearGradient
+      id="elixir-original-c"
+      gradientUnits="userSpaceOnUse"
+      x1="924.646"
+      y1="120.513"
+      x2="924.646"
+      y2="505.851"
+      gradientTransform="matrix(.1227 0 0 .2115 -46.493 17.206)"
+    >
+      <stop offset="0" stopColor="#26053d" stopOpacity=".762" />
+      <stop offset="1" stopColor="#b7b4b4" stopOpacity=".278" />
+    </linearGradient>
+    <linearGradient
+      id="elixir-original-d"
+      gradientUnits="userSpaceOnUse"
+      x1="428.034"
+      y1="198.448"
+      x2="607.325"
+      y2="559.255"
+      gradientTransform="matrix(.1848 0 0 .1404 -42.394 17.138)"
+    >
+      <stop offset="0" stopColor="#91739f" stopOpacity=".46" />
+      <stop offset="1" stopColor="#32054f" stopOpacity=".54" />
+    </linearGradient>
+    <linearGradient
+      id="elixir-original-e"
+      gradientUnits="userSpaceOnUse"
+      x1="907.895"
+      y1="540.636"
+      x2="590.242"
+      y2="201.281"
+      gradientTransform="matrix(.1418 0 0 .1829 -45.23 17.18)"
+    >
+      <stop offset="0" stopColor="#463d49" stopOpacity=".331" />
+      <stop offset="1" stopColor="#340a50" stopOpacity=".821" />
+    </linearGradient>
+    <linearGradient
+      id="elixir-original-f"
+      gradientUnits="userSpaceOnUse"
+      x1="1102.297"
+      y1="100.542"
+      x2="1008.071"
+      y2="431.648"
+      gradientTransform="matrix(.106 0 0 .2448 -47.595 17.242)"
+    >
+      <stop offset="0" stopColor="#715383" stopOpacity=".145" />
+      <stop offset="1" stopColor="#f4f4f4" stopOpacity=".234" />
+    </linearGradient>
+    <linearGradient
+      id="elixir-original-g"
+      gradientUnits="userSpaceOnUse"
+      x1="1354.664"
+      y1="140.06"
+      x2="1059.233"
+      y2="84.466"
+      gradientTransform="matrix(.09173 0 0 .2828 -48.536 17.28)"
+    >
+      <stop offset="0" stopColor="#a5a1a8" stopOpacity=".356" />
+      <stop offset="1" stopColor="#370c50" stopOpacity=".582" />
+    </linearGradient>
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      fill="url(#elixir-original-a)"
+      d="M64.4.5C36.7 13.9 1.9 83.4 30.9 113.9c26.8 33.5 85.4 1.3 68.4-40.5-21.5-36-35-37.9-34.9-72.9z"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      fill="url(#elixir-original-b)"
+      d="M64.4.2C36.8 13.6 1.9 82.9 31 113.5c10.7 12.4 28 16.5 37.7 9.1 26.4-18.8 7.4-53.1 10.4-78.5C68.1 33.9 64.2 11.3 64.4.2z"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      fill="url(#elixir-original-c)"
+      d="M56.7 4.3c-22.3 15.9-28.2 75-24.1 94.2 8.2 48.1 75.2 28.3 69.6-16.5-6-29.2-48.8-39.2-45.5-77.7z"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      fill="url(#elixir-original-d)"
+      d="M78.8 49.8c10.4 13.4 12.7 22.6 6.8 27.9-27.7 19.4-61.3 7.4-54-37.3C22.1 63 4.5 96.8 43.3 101.6c20.8 3.6 54 2 58.9-16.1-.2-15.9-10.8-22.9-23.4-35.7z"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      fill="url(#elixir-original-e)"
+      d="M38.1 36.4c-2.9 21.2 35.1 77.9 58.3 71-17.7 35.6-56.9-21.2-64-41.7 1.5-11 2.2-16.4 5.7-29.3z"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      fill="url(#elixir-original-f)"
+      d="M60.4 49.7c.8 7.9 3.9 20.5 0 28.8S38.7 102 43.6 115.3c11.4 24.8 37.1-4.4 36.9-19 1.1-11.8-6.6-38.7-1.8-52.5L76.5 41l-13.6-4c-2.2 3.2-3 7.5-2.5 12.7z"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      fill="url(#elixir-original-g)"
+      d="M65.3 10.8C36 27.4 48 53.4 49.3 81.6l19.1-55.4c-1.4-5.7-2.3-9.5-3.1-15.4z"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      fill="#330A4C"
+      fillOpacity=".316"
+      d="M68.3 26.1c-14.8 11.7-14.1 31.3-18.6 54 8.1-21.3 4.1-38.2 18.6-54z"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      fill="#FFF"
+      d="M45.8 119.7c8 1.1 12.1 2.2 12.5 3 .3 4.2-11.1 1.2-12.5-3z"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      fill="#EDEDED"
+      fillOpacity=".603"
+      d="M49.8 10.8c-6.9 7.7-14.4 21.8-18.2 29.7-1 6.5-.5 15.7.6 23.5.9-18.2 7.5-39.2 17.6-53.2z"
+    />
+  </svg>
+);
+
+export default ElixirOriginalIcon;
