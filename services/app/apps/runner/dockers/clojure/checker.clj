1d902f779976967a10389db1d477f10acc86914b
Соломатин Артем Николаевич <artem00298@gmail.com>
2024-03-12 11:22:09 +0300

    Refactor checker

--- services/app/apps/runner/dockers/clojure/checker.clj
+++ services/app/apps/runner/dockers/clojure/checker.clj
@@ -3,21 +3,24 @@
 (defn execution-time [start-time]
   (- (System/currentTimeMillis) start-time))
 
+(defn run-tests [solution args]
+  (reduce (fn [acc arg]
+    (let [start-time (System/currentTimeMillis)
+          new-acc (atom {})
+          output
+      (with-out-str
+        (try
+          (swap! new-acc #(merge % {:type "result" :value (apply solution arg) :time (execution-time start-time)}))
+        (catch Exception ex
+          (swap! new-acc #(merge % {:type "error" :value (.getMessage ex) :time (execution-time start-time)})))))]
+      (conj acc (merge {:output output} @new-acc)))) [] args))
+
 (try
   (let [data (json/parse-string (slurp "check/asserts.json"))
         args (data "arguments")]
-  (do
-    (load-file "check/solution.clj")
-    (let [solution-fn (resolve 'solution)]
-      (println (json/generate-string (reduce (fn [acc arg]
-        (let [start-time (System/currentTimeMillis)
-              new-acc (atom {})
-              output (clojure.string/trim (with-out-str (try
-            (swap! new-acc #(merge % {:type "result" :value (apply solution-fn arg) :time (execution-time start-time)}))
-        (catch Exception ex
-            (swap! new-acc #(merge % {:type "error" :value (.getMessage ex) :time (execution-time start-time)}))))))]
-        (conj acc (merge {:output output} @new-acc)))) [] args))))))
-  (catch Exception ex
-    (do
-      (println (json/generate-string {:type "error" :time 0 :value (.getMessage ex)})
-      (System/exit 0)))))
+      (load-file "check/solution.clj")
+      (let [solution-fn (resolve 'solution)]
+        (println (json/generate-string (run-tests solution-fn args)))))
+    (catch Exception ex
+        (println (json/generate-string {:type "error" :time 0 :value (.getMessage ex)}))
+        (System/exit 0)))
