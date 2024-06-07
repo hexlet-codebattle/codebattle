2909a36946fbd58089c4b7af45053d4cbabf3bac
ReDBrother <bataevdmitriy@gmail.com>
2024-06-07 23:21:47 +0300

    fix follow

--- services/app/apps/codebattle/lib/codebattle_web/channels/main_channel.ex
+++ services/app/apps/codebattle/lib/codebattle_web/channels/main_channel.ex
@@ -36,7 +36,6 @@ defmodule CodebattleWeb.MainChannel do
     if socket.assigns.current_user.is_guest do
       {:noreply, socket}
     else
-      Codebattle.PubSub.unsubscribe("user:#{user_id}")
       Codebattle.PubSub.subscribe("user:#{user_id}")
       active_game_id = Game.Context.get_active_game_id(user_id)
       {:reply, {:ok, %{active_game_id: active_game_id, follow_id: user_id}}, socket}
