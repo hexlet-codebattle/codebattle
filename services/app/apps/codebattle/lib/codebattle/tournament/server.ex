7215de64295a6321c2ef4ea38c3f915597e13cde
vtm9 <vtmilyakov@gmail.com>
2024-06-08 06:15:13 +0200

    Close pasting to editor

--- services/app/apps/codebattle/lib/codebattle/tournament/server.ex
+++ services/app/apps/codebattle/lib/codebattle/tournament/server.ex
@@ -304,6 +304,7 @@ defmodule Codebattle.Tournament.Server do
         }
       ) do
     Logger.error("WR matchingstarted")
+
     players =
       tournament
       |> Tournament.Players.get_players("matchmaking_active")
