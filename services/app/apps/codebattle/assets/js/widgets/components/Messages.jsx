cc01885569eb94768c47ddd6128592bcc1c406e0
Titonatos <titanium4ik777@gmail.com>
2025-02-23 15:59:53 +0300

    Fix issue #1979 autoscroll

--- services/app/apps/codebattle/assets/js/widgets/components/Messages.jsx
+++ services/app/apps/codebattle/assets/js/widgets/components/Messages.jsx
@@ -1,5 +1,6 @@
-import React, { useRef, useLayoutEffect } from 'react';
+import React, { useRef, useLayoutEffect, useState } from 'react';
 
+import cn from 'classnames';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import useStayScrolled from '../utils/useStayScrolled';
@@ -16,13 +17,36 @@ const getKey = (id, time, name, index) => {
 
 function Messages({ messages, displayMenu = () => {}, disabled = false }) {
   const listRef = useRef();
-
-  const { stayScrolled /* , scrollBottom */ } = useStayScrolled(listRef);
+  const minScrollHeight = 20;
+  const [scrollHeight, setScrollHeight] = useState(0);
+  const [isScrollButtonVisible, setIsScrollButtonVisible] = useState(false);
+  const { stayScrolled, scrollBottom } = useStayScrolled(listRef);
   // Typically you will want to use stayScrolled or scrollBottom inside
   // useLayoutEffect, because it measures and changes DOM attributes (scrollTop) directly
   useLayoutEffect(() => {
-    stayScrolled();
-  }, [messages.length, stayScrolled]);
+    if (scrollHeight > minScrollHeight) {
+      stayScrolled();
+      setIsScrollButtonVisible(true);
+    } else {
+      scrollBottom();
+      setIsScrollButtonVisible(false);
+    }
+  }, [messages.length, stayScrolled, scrollBottom]);
+
+  const scrollHandler = e => {
+    const chatContainer = e.target;
+    const chatScrollHeight = chatContainer.scrollHeight - chatContainer.scrollTop - chatContainer.clientHeight;
+
+    if (chatScrollHeight < minScrollHeight) {
+      setIsScrollButtonVisible(false);
+    }
+
+    setScrollHeight(chatScrollHeight);
+  };
+
+  const scrollButtonClass = cn('scroll-button', 'position-absolute', 'rounded-circle', 'bg-secondary', 'p-0', 'border-0', {
+    invisible: !isScrollButtonVisible,
+  });
 
   if (disabled) {
     return (
@@ -40,6 +64,7 @@ function Messages({ messages, displayMenu = () => {}, disabled = false }) {
       <ul
         ref={listRef}
         className="overflow-auto pt-0 pl-3 pr-2 position-relative cb-messages-list flex-grow-1"
+        onScroll={scrollHandler}
       >
         {messages.map((message, index) => {
           const {
@@ -62,6 +87,12 @@ function Messages({ messages, displayMenu = () => {}, disabled = false }) {
           );
         })}
       </ul>
+      <button
+        type="button"
+        className={scrollButtonClass}
+        onClick={scrollBottom}
+        aria-label="Scroll to bottom"
+      />
     </>
   );
 }
