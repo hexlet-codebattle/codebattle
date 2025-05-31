883bb35090093125ada35a1b7d0ffe7f8b4ffb87
vtm <vtmilyakov@gmail.com>
2025-05-31 09:52:54 +0200

    Fix

--- services/app/apps/codebattle/lib/codebattle/tournament/storage/ranking.ex
+++ services/app/apps/codebattle/lib/codebattle/tournament/storage/ranking.ex
@@ -27,12 +27,10 @@ defmodule Codebattle.Tournament.Storage.Ranking do
   end
 
   def put_single_record(tournament, place, record) do
-    try do
-      :ets.insert(tournament.ranking_table, {place, record.id, record})
-    rescue
-      _e ->
-        IO.inspect({place, record.id, record}, label: "Error inserting single ranking record")
-    end
+    :ets.insert(tournament.ranking_table, {place, record.id, record})
+  rescue
+    _e ->
+      IO.inspect({place, record.id, record}, label: "Error inserting single ranking record")
   end
 
   def drop_player(tournament, player_id) do
@@ -47,59 +45,49 @@ defmodule Codebattle.Tournament.Storage.Ranking do
   end
 
   def get_first(tournament, limit) do
-    try do
-      :ets.select(tournament.ranking_table, [
-        {{:"$1", :_, :"$3"}, [{:>=, :"$1", 1}, {:"=<", :"$1", limit}], [:"$3"]}
-      ])
-    rescue
-      _e ->
-        IO.inspect({tournament, limit}, label: "Error getting first ranking records")
-        []
-    end
+    :ets.select(tournament.ranking_table, [
+      {{:"$1", :_, :"$3"}, [{:>=, :"$1", 1}, {:"=<", :"$1", limit}], [:"$3"]}
+    ])
+  rescue
+    _e ->
+      IO.inspect({tournament, limit}, label: "Error getting first ranking records")
+      []
   end
 
   def get_all(tournament) do
-    try do
-      :ets.select(tournament.ranking_table, [{{:_, :_, :"$3"}, [], [:"$3"]}])
-    rescue
-      _e ->
-        IO.inspect(tournament, label: "Error getting all ranking records")
-        []
-    end
+    :ets.select(tournament.ranking_table, [{{:_, :_, :"$3"}, [], [:"$3"]}])
+  rescue
+    _e ->
+      IO.inspect(tournament, label: "Error getting all ranking records")
+      []
   end
 
   def get_by_id(tournament, id) do
-    try do
-      case :ets.select(tournament.ranking_table, [{{:_, :"$2", :"$3"}, [{:==, :"$2", id}], [:"$3"]}]) do
-        [ranking_entity] -> ranking_entity
-        [] -> nil
-      end
-    rescue
-      _e ->
-        IO.inspect({tournament, id}, label: "Error getting ranking record by id")
-        nil
+    case :ets.select(tournament.ranking_table, [{{:_, :"$2", :"$3"}, [{:==, :"$2", id}], [:"$3"]}]) do
+      [ranking_entity] -> ranking_entity
+      [] -> nil
     end
+  rescue
+    _e ->
+      IO.inspect({tournament, id}, label: "Error getting ranking record by id")
+      nil
   end
 
   def count(tournament) do
-    try do
-      :ets.select_count(tournament.ranking_table, [{:_, [], [true]}])
-    rescue
-      _e ->
-        IO.inspect(tournament, label: "Error counting ranking records")
-        0
-    end
+    :ets.select_count(tournament.ranking_table, [{:_, [], [true]}])
+  rescue
+    _e ->
+      IO.inspect(tournament, label: "Error counting ranking records")
+      0
   end
 
   def get_slice(tournament, start_place, end_place) do
-    try do
-      :ets.select(tournament.ranking_table, [
-        {{:"$1", :_, :"$3"}, [{:>=, :"$1", start_place}, {:"=<", :"$1", end_place}], [:"$3"]}
-      ])
-    rescue
-      _e ->
-        IO.inspect({tournament, start_place, end_place}, label: "Error getting slice of ranking records")
-        []
-    end
+    :ets.select(tournament.ranking_table, [
+      {{:"$1", :_, :"$3"}, [{:>=, :"$1", start_place}, {:"=<", :"$1", end_place}], [:"$3"]}
+    ])
+  rescue
+    _e ->
+      IO.inspect({tournament, start_place, end_place}, label: "Error getting slice of ranking records")
+      []
   end
 end
