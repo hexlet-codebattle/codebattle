ab03ce5faf8a9d783630f10bfd0498a0612fde0a
Vitaly <vtmilyakov@gmail.com>
2026-02-19 13:15:14 +0100

    Fix pool size

--- apps/codebattle/lib/codebattle/utils/release.ex
+++ apps/codebattle/lib/codebattle/utils/release.ex
@@ -7,12 +7,23 @@ defmodule Codebattle.Utils.Release do
     Application.ensure_all_started(:ssl)
 
     for repo <- repos() do
-      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
+      {:ok, _, _} =
+        Ecto.Migrator.with_repo(
+          repo,
+          &Ecto.Migrator.run(&1, :up, all: true),
+          pool_size: migration_pool_size()
+        )
     end
   end
 
   defp repos do
     Application.load(@app)
     Application.fetch_env!(@app, :ecto_repos)
+  end
+
+  defp migration_pool_size do
+    "CODEBATTLE_MIGRATION_POOL_SIZE"
+    |> System.get_env("2")
+    |> String.to_integer()
   end
 end
