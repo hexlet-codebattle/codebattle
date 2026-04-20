f22ebde8a1226f933edd7d932185fe3e71ba035f
Vitaly <vtmilyakov@gmail.com>
2026-04-20 12:36:18 +0200

    Fix

--- apps/codebattle/test/codebattle_web/plugs/restrict_access_test.exs
+++ apps/codebattle/test/codebattle_web/plugs/restrict_access_test.exs
@@ -17,7 +17,7 @@ defmodule CodebattleWeb.Plugs.RescrictAccessTest do
     assert conn.status == 200
   end
 
-  test "allows tournaments with ids from 2 to 22 in mini mode", %{conn: conn} do
+  test "allows tournaments with ids from 2 to 31 in mini mode", %{conn: conn} do
     user = insert(:user)
     insert(:tournament, id: 2, creator_id: user.id)
 
@@ -29,8 +29,13 @@ defmodule CodebattleWeb.Plugs.RescrictAccessTest do
     assert conn.status != 302
   end
 
-  test "blocks tournaments with ids outside 2 to 22 in mini mode", %{conn: conn} do
-    conn = get(conn, "/tournaments/23")
+  test "blocks tournaments with ids outside 2 to 31 in mini mode", %{conn: conn} do
+    user = insert(:user)
+
+    conn =
+      conn
+      |> put_session(:user_id, user.id)
+      |> get("/tournaments/32")
 
     assert conn.status == 302
     assert redirected_to(conn) == "/"
